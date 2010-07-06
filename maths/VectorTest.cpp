d1c4fff8ae4b0a31d091c89b94abaf547a42eab7
Nicholas Chapman <nick@indigorenderer.com>
2010-07-06 01:08:23 +0000

    * Fixed bug where render would stop if IGI or PNG autosaving failed to write to disk. (nick) * Fixed bug where background would render incompletely or not at all. (nick) * Made world do explicit, inline test against background sphere in traceRay(). (nick)
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/branches/2_4_new@1069 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- maths/VectorTest.cpp
+++ maths/VectorTest.cpp
@@ -7,32 +7,38 @@ Generated at Tue May 25 16:47:39 +1200 2010
 #include "VectorTest.h"
 
 
+//#include "../utils/timer.h"
+//#include "../utils/stringutils.h"
+//#include "../indigo/globals.h"
+
+
 #define VEC3_INLINE __forceinline
 
 
-template <class Real>
+//template <class Real>
+typedef float TVReal;
 class TestVec3
 {
 public:
 
-	VEC3_INLINE TestVec3()
+	__declspec(nothrow) VEC3_INLINE TestVec3() throw ()
 	{}
 
-	VEC3_INLINE ~TestVec3()
+	__declspec(nothrow) VEC3_INLINE ~TestVec3() throw ()
 	{}
 
-	VEC3_INLINE TestVec3(Real x_, Real y_, Real z_)
+	__declspec(nothrow) VEC3_INLINE TestVec3(TVReal x_, TVReal y_, TVReal z_) throw ()
 	:	x(x_),
 		y(y_),
 		z(z_)
 	{}
 
-	VEC3_INLINE const TestVec3 operator * (Real factor) const
+	VEC3_INLINE const TestVec3 operator * (TVReal factor) const throw ()
 	{
 		return TestVec3(x * factor, y * factor, z * factor);
 	}
 
-	VEC3_INLINE TestVec3& operator += (const TestVec3& rhs)
+	VEC3_INLINE TestVec3& operator += (const TestVec3& rhs) throw ()
 	{
 		x += rhs.x;
 		y += rhs.y;
@@ -40,7 +46,7 @@ public:
 		return *this;
 	}
 
-	Real x,y,z;
+	TVReal x,y,z;
 };
 
 
@@ -56,39 +62,42 @@ VectorTest::~VectorTest()
 }
 
 
-//__declspec(nothrow)
-static VEC3_INLINE TestVec3<float> testVMul(const TestVec3<float>& a, float f)
+__declspec(nothrow)
+static VEC3_INLINE const TestVec3 testVMul(const TestVec3& a, TVReal f) throw ()
 {
-	//return Vec3f(a.x * f, a.y * f, a.z * f);
-	TestVec3<float> res;
+	return TestVec3(a.x * f, a.y * f, a.z * f);
+	/*TestVec3 res;
 	res.x = a.x * f;
 	res.y = a.y * f;
 	res.z = a.z * f;
-	return res;
+	return res;*/
 }
 
 
 float VectorTest::test()
 {
-	TestVec3<float> a(1.f, 2.f, 3.f);
+	TestVec3 a(1.f, 2.f, 3.f);
 
 	int N = 10000000;
 
 	// Run a test using methods that return the vec3.
 	{
 		//Timer timer;
-		TestVec3<float> c(0.f, 0.f, 0.f);
+		TestVec3 c(0.f, 0.f, 0.f);
 		for(int i=0; i<N; ++i)
 		{
 			const float factor = (float)i;
-			const TestVec3<float> temp = testVMul(a, factor);
+			const TestVec3 temp = testVMul(a, factor);
 			c += temp;
 		}
 
-		//conPrint(c.toString());
-		//conPrint(timer.elapsedString());
+		/*conPrint(toString(c.x));
+		conPrint(toString(c.y));
+		conPrint(toString(c.z));
+		conPrint(timer.elapsedString());*/
 		return c.x;
 	}
 
+//	exit(0);
 	
 }
