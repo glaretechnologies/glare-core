3d5a181616b1242ae0018b0735e8e524d738bd56
Thomas Ludwig <thomas.ludwig@gmail.com>
2010-07-23 05:58:24 +0000

    Faster realtime camera updating via new pause threads message. Made PathTraceRenderer reset properly - clear splat buffer, reset Sobol sampler.
    Changed alignment of BatchedRayTracer stuff to be cacheline size (64b) from 16b.
    Changed alignment of BVH nodes to be cacheline size.
    Took old code out of BatchedPathTracer, maybe a bit faster.
    Added reset() method to CoreSampleGenerator to enable faster resetting than delete+new.
    
    
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/branches/2_4_new@1136 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ utils/PauseThreadMessage.h
@@ -0,0 +1,39 @@
+/*=====================================================================
+PauseThreadMessage.h
+-------------------
+Copyright Glare Technologies Limited 2010 -
+Generated at Thu Jul 22 13:00:48 +1200 2010
+=====================================================================*/
+#pragma once
+
+
+#include "ThreadMessage.h"
+#include "mutex.h"
+#include "Condition.h"
+
+
+/*=====================================================================
+PauseThreadMessage
+------------------
+=====================================================================*/
+class PauseThreadMessage : public ThreadMessage
+{
+public:
+	/*=====================================================================
+	PauseThreadMessage
+	------------------
+
+	=====================================================================*/
+	PauseThreadMessage(Mutex* _num_paused_threads_mutex, int* _num_paused_threads, Condition* _all_threads_paused);
+
+	virtual ~PauseThreadMessage();
+
+	virtual ThreadMessage* clone() const { return new PauseThreadMessage(num_paused_threads_mutex, num_paused_threads, all_threads_paused); }
+
+	virtual const std::string debugName() const { return "PauseThreadMessage"; }
+
+//private:
+	Mutex* num_paused_threads_mutex;
+	int* num_paused_threads;
+	Condition* all_threads_paused;
+};
