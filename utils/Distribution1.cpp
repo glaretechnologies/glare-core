c7ded9d48e1c56defc9bc9ef7a5855b18ae2e36c
Thomas Ludwig <thomas.ludwig@gmail.com>
2010-08-21 22:32:58 +0000

    Fixed bug in Distribution1 test
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@1309 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- utils/Distribution1.cpp
+++ utils/Distribution1.cpp
@@ -14,55 +14,52 @@ Generated at Sun Aug 15 02:19:14 +1200 2010
 #include "MTwister.h"
 #include "timer.h"
 
+template<>
 void Distribution1<float>::test()
 {
-	int num_bins = 10;
+	int num_bins = 16;
 
 	std::vector<uint32> bins(num_bins);
-	std::vector<float> weights(num_bins);
+	std::vector<uint32> resampled_bins(num_bins);
 
+	std::vector<float> pdf(num_bins);
 	for(int i = 0; i < num_bins; ++i)
 	{
-		bins[i] = 0;
-		weights[i] = 1.0f / (float)num_bins;
+		bins[i] = resampled_bins[i] = 0;
+		pdf[i] = 1;
 	}
+	pdf[num_bins / 2] = 10;
 
-	weights[num_bins / 2] *= 10;
 
-	Distribution1 dist(weights);
+	Distribution1 dist(pdf, 0.5f);
 
-	uint32 num_samples = 4294967296 / 16 - 1;
 
+	uint32 num_samples = num_bins * 768 * 64;
 	MTwister rng(631579123);
 
 
 	Timer timer;
 	timer.reset();
 
-	for(uint32 i = 0; i < num_samples; ++i) bins[dist.sample(rng.unitRandom())]++;
+	for(uint32 i = 0; i < num_samples; ++i)
+	{
+		float u = rng.unitRandom();
+		float resampled_u;
+
+		bins[dist.sampleLerp(u, resampled_u)]++;
+		resampled_bins[((resampled_u == 1) ? num_bins - 1 : (uint32)(resampled_u * num_bins))]++;
+	}
 
 	double time_taken = timer.elapsed();
 
 
-	if(false)
+	if(true)
 	{
-	std::cout << "histogram:" << std::endl;
-	for(int i = 0; i < num_bins; ++i)
-		std::cout << "bin " << i << ": " << bins[i] << std::endl;
+		for(int i = 0; i < num_bins; ++i) std::cout << "bin " << i << ":\t\t\t" << bins[i] << std::endl;
+		for(int i = 0; i < num_bins; ++i) std::cout << "bin resampled " << i << ":\t" << resampled_bins[i] << std::endl;
 	}
 
 	std::cout << "simulation took " << time_taken << " seconds." << std::endl;
-
-
-	double errorsum[4] = { 0, 0, 0, 0 };
-	for (uint32 i = 0; i < num_samples; ++i)
-	{
-		float x01_out, x_in = rng.unitRandom();
-		dist.sampleLerp(x_in, x01_out);
-		errorsum[i & 3] += fabs(x01_out - x_in);
-	}
-
-	std::cout << "sum of renomalisation errors: " << fabs(0.25 * (errorsum[0] + errorsum[1] + errorsum[2] + errorsum[3])) << std::endl;
 }
 
 #endif
--- utils/Distribution1.h
+++ utils/Distribution1.h
@@ -23,48 +23,47 @@ public:
 	 Distribution1() { }
 	~Distribution1() { }
 
-	Distribution1(const std::vector<Real>& pdf)
+	Distribution1(const std::vector<Real>& pdf, float guide_table_size_scale = 1)
 	{
 		const uint32 table_size = (uint32)pdf.size();
 
 		cdfTable.resize(table_size);
-		pdfInvTable.resize(table_size);
-		guideTable.resize(table_size + 1);
+		guideTable.resize(table_size * guide_table_size_scale);
 
 		// Compute PDF normalisation factor using 4 partial sums.
 		double running_cdf[4] = { 0, 0, 0, 0 };
 		for(uint32 i = 0; i < table_size; ++i) running_cdf[i & 3] += pdf[i];
 
-		// Construct normalised CDF table
+		// Construct normalised CDF and inverse PDF tables.
 		const double CDF_normalise = 1 / (running_cdf[0] + running_cdf[1] + running_cdf[2] + running_cdf[3]);
 		running_cdf[0] = running_cdf[1] = running_cdf[2] = running_cdf[3] = 0;
 		for(uint32 i = 0; i < table_size; ++i)
 		{
 			running_cdf[i & 3] += pdf[i];
-
 			cdfTable[i] = (Real)((running_cdf[0] + running_cdf[1] + running_cdf[2] + running_cdf[3]) * CDF_normalise);
-			pdfInvTable[i] = (Real)(pdf[i] * CDF_normalise);
 		}
 
 		// Build the guide table.
 		for(uint32 i = 0, j = 0; i < guideTable.size(); ++i)
 		{
 			// Find the largest j st. CDF[j] < target CDF[i]
-			const double target_cdf = i / (double)(guideTable.size() - 1);
-			while(cdfTable[j] < target_cdf) ++j;
+			const double target_cdf = i / (double)guideTable.size();
+			while(cdfTable[j] <= target_cdf) ++j;
 			guideTable[i] = j;
 		}
 	}
 
 	inline uint32 sample(const Real x) const
 	{
-		uint32 i = guideTable[(uint32)(x * (guideTable.size() - 1))]; while(cdfTable[i] < x) ++i; assert(i < cdfTable.size());
+		if(x == 1) return cdfTable.size() - 1;
+		uint32 i = guideTable[(uint32)(x * guideTable.size())]; while(cdfTable[i] < x) ++i; assert(i < cdfTable.size());
 		return i;
 	}
 
 	inline uint32 sampleLerp(const Real x, Real& x_lerp) const
 	{
-		uint32 i = sample(x); x_lerp = (cdfTable[i] - x) * pdfInvTable[i]; assert(x_lerp >= 0 && x_lerp <= 1);
+		uint32 i = sample(x);
+		x_lerp = (i < 1) ? x / cdfTable[0] : (cdfTable[i] - x) / (cdfTable[i] - cdfTable[i - 1]); assert(x_lerp >= 0 && x_lerp <= 1);
 		return i;
 	}
 
@@ -73,6 +72,5 @@ public:
 private:
 
 	js::Vector<Real, 64> cdfTable;
-	js::Vector<Real, 64> pdfInvTable;
 	js::Vector<Real, 64> guideTable;
 };
