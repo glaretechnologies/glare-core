f87ca4f7e2014081e0697041a9435d33f3c5e35f
Nicholas Chapman <nick@indigorenderer.com>
2010-10-22 01:36:42 +0000

    Added NetworkingTests
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@1552 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ networking/NetworkingTests.cpp
@@ -0,0 +1,65 @@
+/*=====================================================================
+NetworkingTests.cpp
+-------------------
+Copyright Glare Technologies Limited 2010 -
+Generated at Fri Oct 22 13:53:30 +1300 2010
+=====================================================================*/
+#include "NetworkingTests.h"
+
+
+#include "mysocket.h"
+#include "networking.h"
+
+
+NetworkingTests::NetworkingTests()
+{
+
+}
+
+
+NetworkingTests::~NetworkingTests()
+{
+
+}
+
+
+#if BUILD_TESTS
+
+
+#include "../indigo/globals.h"
+
+
+void NetworkingTests::test()
+{
+	conPrint("NetworkingTests::test()");
+
+	const int N = 1;
+
+	for(int i=0; i<N; ++i)
+	{
+		try
+		{
+			conPrint("Connecting to localhost:6666");
+			MySocket sock("localhost", 6666);
+		}
+		catch(MySocketExcep& )
+		{
+
+		}
+	}
+
+	for(int i=0; i<N; ++i)
+	{
+		try
+		{
+			conPrint("Connecting to not_a_resolvable_dns_name:6666");
+			MySocket sock("not_a_resolvable_dns_name", 6666);
+		}
+		catch(MySocketExcep& )
+		{
+
+		}
+	}
+	
+}
+#endif
