116bd2ac1d5546ca69da4d83be9c9f8f4e1c9ff4
Thomas Ludwig <thomas.ludwig@gmail.com>
2010-11-01 06:23:20 +0000

    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@1603 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ utils/zKey.h
@@ -0,0 +1,56 @@
+/*=====================================================================
+zKey.h
+------
+Copyright Glare Technologies Limited 2010 -
+Generated at Fri Oct 29 14:08:44 +1300 2010
+=====================================================================*/
+#pragma once
+
+
+#include "platform.h"
+
+
+/*=====================================================================
+zKey
+----
+
+=====================================================================*/
+template<int dimensions>
+class zKey
+{
+public:
+	zKey(const uint32_t _coords[dimensions], const uint32_t _val)
+	{
+		computeKey(_coords);
+		val = _val;
+	}
+
+	~zKey();
+
+	uint32_t key[dimensions];
+	uint32_t val;
+
+	void computeKey(const uint32_t _coords[dimensions])
+	{
+		for(uint32_t d = 0; d < dimensions; ++d) key[d] = 0;
+
+		for(uint32_t d = 0; d < dimensions; ++d)
+			interleaveBits(d, _coords[d]);
+	}
+
+	inline bool operator < (const zKey& rhs) const
+	{
+		for(int d = dimensions - 1; d >= 0; --d)
+			if(key[d] != rhs.key[d]) return key[d] < rhs.key[d];
+		return false;
+	}
+
+private:
+
+	inline void interleaveBits(const uint32_t dim_idx, const uint32_t dim_val)
+	{
+		for(uint32_t i = 0; i < 32; ++i)
+			key[(i * dimensions + dim_idx) / 32] |= ((dim_val >> i) & 1) << ((i * dimensions + dim_idx) % 32);
+	}
+
+};
