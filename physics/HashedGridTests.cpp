16e653eb4b32f173da22d076c54ff2c4b57a9045
Thomas Ludwig <thomas.ludwig@gmail.com>
2010-11-26 05:48:02 +0000

    Started on skip list creation for OpenCL Finally checked in new HashedGridTests
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@1718 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- physics/HashedGridTests.cpp
+++ physics/HashedGridTests.cpp
@@ -10,6 +10,7 @@ Generated at Thu Sep 09 12:53:49 +1200 2010
 #if (BUILD_TESTS)
 
 #include "HashedGrid.h"
+//#include "HashedGridSTL.h"
 
 #include <iostream>
 #include "../utils/clock.h"
@@ -30,17 +31,24 @@ HashedGridTests::~HashedGridTests()
 
 void HashedGridTests::test()
 {
-	int num_grid_resolutions = 7;
-	int grid_resolutions[] = { 3, 4, 8, 16, 64, 1024, 4096 };
+	int num_grid_resolutions = 6;
+	int grid_resolutions[] = { 3, 4, 8, 16, 64, 1024 };
 
-	int num_grid_sizes = 6;
-	float grid_sizes[] = { 0.1f, 1.0f, 16.0f, 64.0f, 1024.0f, 65536.0f };
+	int num_grid_sizes = 5;
+	float grid_sizes[] = { 0.1f, 1.0f, 16.0f, 1024.0f, 65536.0f };
 
+	int num_insertion_scales = 4;
+	float insertion_scales[] = { 0.3f, 1.0f, 1.4f, 3.1f };
+
+	for(int insertion_scale_count = 0; insertion_scale_count < num_insertion_scales; ++insertion_scale_count)
 	for(int grid_res_count = 0; grid_res_count < num_grid_resolutions; ++grid_res_count)
 	for(int grid_size_count = 0; grid_size_count < num_grid_sizes; ++grid_size_count)
 	{
+		const float insertion_scale = insertion_scales[insertion_scale_count];
 		const int grid_res = grid_resolutions[grid_res_count];
 		const float grid_size = grid_sizes[grid_size_count];
+
+		if(insertion_scale_count != 3) continue; // TEMP HACK
 
 		js::AABBox aabb;
 		aabb.min_ = Vec4f(-grid_size, -grid_size, -grid_size, 1);
@@ -50,9 +58,11 @@ void HashedGridTests::test()
 
 		Vec4f insertion_point;
 		js::AABBox insert_aabb;
-		HashedGrid<int, true> grid(aabb, grid_res, grid_res, grid_res);
 
-		std::cout << "testing grid resolution " << grid_res << "^3, size = " << grid_size << "^3" << ", num buckets = " << grid.getNumBuckets() << std::endl;
+		HashedGrid<int, true> grid(aabb, grid_res, grid_res, grid_res);
+		//HashedGridSTL<int, true> grid(aabb, grid_res, grid_res, grid_res);
+
+		std::cout << "testing grid resolution " << grid_res << "^3, size = " << grid_size << "^3, insertion box scale = " << insertion_scale << std::endl;
 
 
 		// --------------------------------------------------------------------------------------------------------
@@ -65,10 +75,13 @@ void HashedGridTests::test()
 		for(int interior_x = 1; interior_x < (grid_res - 1); ++interior_x)
 		{
 			insertion_point = aabb.min_ + Vec4f(interior_x , interior_y, interior_z, 0) * grid_cell_width;
+			//std::cout << "insertion point is (" << insertion_point.x[0] << ", " << insertion_point.x[1] << ", " << insertion_point.x[2] << ", " << ")" << std::endl;
 
-			// Bounding box is two grid cells wide in each dimension, centred around the insertion point
-			insert_aabb.min_ = insertion_point - Vec4f(grid_cell_width, grid_cell_width, grid_cell_width, 0);
-			insert_aabb.max_ = insertion_point + Vec4f(grid_cell_width, grid_cell_width, grid_cell_width, 0);
+			const float aabb_size = grid_cell_width * insertion_scale;
+
+			// Bounding box is two grid cells wide in each dimension (scaled by insertion_scale), centred around the insertion point
+			insert_aabb.min_ = insertion_point - Vec4f(aabb_size, aabb_size, aabb_size, 0);
+			insert_aabb.max_ = insertion_point + Vec4f(aabb_size, aabb_size, aabb_size, 0);
 
 			// Insert value with associated bound into grid
 			grid.clear();
@@ -80,10 +93,18 @@ void HashedGridTests::test()
 			for(int y = 0; y < grid_res; ++y)
 			for(int x = 0; x < grid_res; ++x)
 			{
+				//HashBucketSTL<int>& bucket = grid.getBucketForIndices(x, y, z);
 				HashBucket<int>& bucket = grid.getBucketForIndices(x, y, z);
 
-				const int x_minbound = (int)(interior_x - 1), y_minbound = (int)(interior_y - 1), z_minbound = (int)(interior_z - 1);
-				const int x_maxbound = (int)(interior_x + 1), y_maxbound = (int)(interior_y + 1), z_maxbound = (int)(interior_z + 1);
+				const int
+					x_minbound = myMax<int>(0, (int)(interior_x - insertion_scale)),
+					y_minbound = myMax<int>(0, (int)(interior_y - insertion_scale)),
+					z_minbound = myMax<int>(0, (int)(interior_z - insertion_scale));
+
+				const int
+					x_maxbound = myMin<int>(grid_res - 1, (int)(interior_x + insertion_scale)),
+					y_maxbound = myMin<int>(grid_res - 1, (int)(interior_y + insertion_scale)),
+					z_maxbound = myMin<int>(grid_res - 1, (int)(interior_z + insertion_scale));
 
 				const bool should_have_data =	(x >= x_minbound) && (x <= x_maxbound) &&
 												(y >= y_minbound) && (y <= y_maxbound) &&
@@ -102,7 +123,7 @@ void HashedGridTests::test()
 
 		// Now we test with random insertion points
 
-		int num_random_insertions = grid_res * grid_res * grid_res * 64;
+		int num_random_insertions = grid_res * grid_res * grid_res * 4;//64;
 		std::cout << "testing insertion at " << num_random_insertions << " random points." << std::endl;
 
 		MTwister rng(::getSecsSince1970());
@@ -115,9 +136,11 @@ void HashedGridTests::test()
 
 			insertion_point = aabb.min_ + Vec4f(insertion_x, insertion_y, insertion_z, 0) * grid_cell_width;
 
+			const float aabb_size = grid_cell_width * insertion_scale;
+
 			// Bounding box is one grid cell big, centred around the insertion point
-			insert_aabb.min_ = insertion_point - Vec4f(grid_cell_width, grid_cell_width, grid_cell_width, 0);
-			insert_aabb.max_ = insertion_point + Vec4f(grid_cell_width, grid_cell_width, grid_cell_width, 0);
+			insert_aabb.min_ = insertion_point - Vec4f(aabb_size, aabb_size, aabb_size, 0);
+			insert_aabb.max_ = insertion_point + Vec4f(aabb_size, aabb_size, aabb_size, 0);
 
 			// Insert value with associated bound into grid
 			grid.clear();
@@ -128,10 +151,11 @@ void HashedGridTests::test()
 			for(int y = 0; y < grid_res; ++y)
 			for(int x = 0; x < grid_res; ++x)
 			{
+				//HashBucketSTL<int>& bucket = grid.getBucketForIndices(x, y, z);
 				HashBucket<int>& bucket = grid.getBucketForIndices(x, y, z);
 
-				const int x_minbound = (int)(insertion_x - 1), y_minbound = (int)(insertion_y - 1), z_minbound = (int)(insertion_z - 1);
-				const int x_maxbound = (int)(insertion_x + 1), y_maxbound = (int)(insertion_y + 1), z_maxbound = (int)(insertion_z + 1);
+				const int x_minbound = (int)(insertion_x - insertion_scale), y_minbound = (int)(insertion_y - insertion_scale), z_minbound = (int)(insertion_z - insertion_scale);
+				const int x_maxbound = (int)(insertion_x + insertion_scale), y_maxbound = (int)(insertion_y + insertion_scale), z_maxbound = (int)(insertion_z + insertion_scale);
 
 				const bool should_have_data =	(x >= x_minbound) && (x <= x_maxbound) &&
 												(y >= y_minbound) && (y <= y_maxbound) &&
