8874f82afe679ec5a82006f9d8649d0b3f49d0a2
Nicholas Chapman <nick@indigorenderer.com>
2011-02-09 21:20:57 +0000

    Added missing MVector, added some stuff to TODO.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@1906 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ maths/MVector.cpp
@@ -0,0 +1,119 @@
+#include "MVector.h"
+
+
+#include "../maths/mathstypes.h"
+
+
+/*
+//template <class double>
+void Vector<double>::test()
+{
+	const double d[] = {0, 1, 2};
+	Vector<double> v(3, d);
+
+	assert(::epsEqual(v.averageValue(), 1.0));
+	assert(::epsEqual(v.variance(), 0.6666666));
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include "C:\programming\graphics2d\graphics2d.h"
+//#include "types.h"//for myMin() etc
+
+#if 0
+void Vector::draw(Graphics2d& graphics, int xpos, int ypos, int maxbaramp, float scale, 
+				  float width, const Vec3& colour) const
+{
+	assert(dimension);
+	//-----------------------------------------------------------------
+	//blank out graph background
+	//-----------------------------------------------------------------
+	graphics.drawRect(Vec2(xpos, ypos-maxbaramp), width, maxbaramp*2, Vec3(0,0,0));
+	/*
+	{	
+	for(int x=xpos; x<xpos + width; x++)
+		for(int y=ypos - maxbaramp; y<ypos + maxbaramp; y++)
+		{
+			graphics.drawPixel(x, y, 0, 0, 0);
+		}
+	}*/
+
+	//-----------------------------------------------------------------
+	//draw the graph
+	//-----------------------------------------------------------------
+	const float BAR_WIDTH = width / (float)dimension;
+
+
+	for(int bar=0; bar<dimension; bar++)
+	{
+		
+		int barleftx = xpos + (int)((float)bar * BAR_WIDTH);
+
+		int barheight = (int)(data[bar] * scale);
+		
+
+		//-----------------------------------------------------------------
+		//clip bar
+		//-----------------------------------------------------------------
+		if(barheight > maxbaramp)
+			barheight = maxbaramp;
+		else if(barheight < -maxbaramp)
+			barheight = -maxbaramp;
+
+		int endbar_y = ypos - barheight;//minus as positive amplitude should go up the screen
+
+		int small_y = myMin(endbar_y, ypos);
+		int big_y = myMax(endbar_y, ypos);
+
+		assert(big_y >= small_y);
+
+		graphics.drawRect(Vec2(barleftx, small_y), BAR_WIDTH, big_y - small_y, colour); 
+		/*for(int x=barleftx; x< barleftx+BAR_WIDTH; x++)//for each vertical set of pixels in the bar
+		{		
+			for(int y=small_y; y<big_y; y++)
+			{
+				graphics.drawPixel(x, y, r, g, b);
+			}
+		
+		}*/
+	}
+
+
+	//-----------------------------------------------------------------
+	//draw lines around the graph
+	//-----------------------------------------------------------------
+
+	//-----------------------------------------------------------------
+	//draw line across top
+	//-----------------------------------------------------------------
+	graphics.drawLine(Vec2(xpos, ypos - maxbaramp), Vec2(xpos + width, ypos - maxbaramp), Vec3(0.3, 0.3, 0.3)); 
+
+	//-----------------------------------------------------------------
+	//draw line across bottom
+	//-----------------------------------------------------------------
+	graphics.drawLine(Vec2(xpos, ypos + maxbaramp), Vec2(xpos + width, ypos + maxbaramp), Vec3(0.3, 0.3, 0.3));
+
+	//-----------------------------------------------------------------
+	//draw line down left hand side
+	//-----------------------------------------------------------------
+	graphics.drawLine(Vec2(xpos, ypos - maxbaramp), Vec2(xpos, ypos + maxbaramp), Vec3(0.3, 0.3, 0.3));
+
+	//-----------------------------------------------------------------
+	//draw line down right hand side
+	//-----------------------------------------------------------------
+	graphics.drawLine(Vec2(xpos + width, ypos - maxbaramp), Vec2( xpos + width, ypos + maxbaramp), Vec3(0.3, 0.3, 0.3));
+}
+#endif
+
---
+++ maths/MVector.h
@@ -0,0 +1,458 @@
+#ifndef __VECTOR_H__
+#define __VECTOR_H__
+
+
+#include "../maths/mathstypes.h"
+#include <cassert>
+#include <vector>
+
+
+template <class Real>
+class MVector
+{
+public:
+	inline MVector(int dimension);
+	inline MVector(int dimension, Real initial_value);
+	inline MVector();
+	inline MVector(int dimension, const Real* data);
+	inline MVector(const MVector& rhs);
+
+	inline ~MVector();
+
+	//aliases:
+//	inline int getDim() const { return dimension; }
+	inline int size() const { return data.size(); }
+//	inline int getSize() const { return dimension; }
+
+	inline Real operator[] (int index) const;
+	inline Real& operator[] (int index);
+
+	inline MVector& operator = (const MVector& rhs);
+	inline bool operator == (const MVector& rhs) const;
+
+
+	inline MVector operator - (const MVector& rhs) const;
+	inline MVector operator + (const MVector& rhs) const;
+
+	inline MVector& operator -= (const MVector& rhs);
+	inline MVector& operator += (const MVector& rhs);
+
+	//inline MVector& addFirstNElements(const Vector& rhs, int N);
+
+	inline MVector operator * (Real factor) const;
+
+	inline MVector& operator *= (Real factor);
+	inline MVector& scale(Real factor);//same thing
+
+	inline MVector operator / (Real factor) const;
+	inline MVector& operator /= (Real factor);
+
+	inline MVector& normalise();
+	inline Real length() const;
+	inline Real length2() const;
+	inline Real dotProduct(const MVector& rhs) const;
+
+	inline Real angle(const MVector& rhs) const;
+	inline Real angleNormized(const MVector& rhs) const;
+
+	inline MVector& zero();
+
+	inline const Real* toReal() const;
+	inline Real* toReal();
+
+	typedef typename std::vector<Real>::iterator iterator;
+	inline iterator begin();
+	inline iterator end();
+
+	typedef typename std::vector<Real>::const_iterator const_iterator;
+	inline const_iterator const_begin() const;
+	inline const_iterator const_end() const;
+
+	inline Real sum() const;
+	inline Real averageValue() const;
+	inline Real variance() const;
+
+	inline Real Min() const;
+	inline Real Max() const;
+
+	//static void test();
+
+private:
+	std::vector<Real> data;
+};
+
+
+template <class Real>
+MVector<Real>::MVector(int dimension_)
+:	data(dimension_)
+{
+}
+
+
+template <class Real>
+MVector<Real>::MVector(int dimension_, Real initial_value)
+:	data(dimension_, initial_value)
+{
+}
+
+template <class Real>
+MVector<Real>::MVector(const MVector& rhs)
+{
+	data = rhs.data;
+}
+
+template <class Real>
+MVector<Real>::MVector()
+{
+}
+
+template <class Real>
+MVector<Real>::MVector(int dimension_, const Real* newdata)
+{
+	assert(newdata);
+	data.resize(dimension_);
+
+	for(int i=0; i<dimension_; i++)
+		data[i] = newdata[i];
+}
+
+
+template <class Real>
+MVector<Real>::~MVector()
+{
+}
+
+
+template <class Real>
+Real MVector<Real>::operator[] (int index) const
+{
+	//assert(index >= 0 && index < dimension);
+	
+	return data[index];
+}
+
+template <class Real>
+Real& MVector<Real>::operator[] (int index)
+{
+	return data[index];
+}
+
+template <class Real>
+MVector<Real>& MVector<Real>::operator = (const MVector& rhs)
+{
+	if(this == &rhs)
+		return *this;
+
+	data = rhs.data;
+	return *this;
+}
+
+
+
+
+template <class Real>
+bool MVector<Real>::operator == (const MVector& rhs) const
+{
+	if(data.size() != rhs.size())
+		return false;
+
+	for(int i=0; i<data.size(); ++i)
+		if(data[i] != rhs.data[i])
+			return false;
+
+	return true;
+}
+
+
+// template <class Real>
+// MVector<Real> MVector<Real>::operator - (const MVector& rhs) const
+// {
+// 	assert(dimension == rhs.dimension);
+// 	MVector newvec(dimension);
+// 
+// 	for(int i=0; i<dimension; i++)
+// 		newvec[i] = data[i] - rhs.data[i];
+// 
+// 	return newvec;
+// }
+
+
+// template <class Real>
+// MVector<Real> MVector<Real>::operator + (const MVector& rhs) const
+// {
+// 	assert(dimension == rhs.dimension);
+// 	MVector newvec(dimension);
+// 
+// 	for(int i=0; i<dimension; i++)
+// 		newvec[i] = data[i] + rhs.data[i];
+// 
+// 	return newvec;
+// }
+
+
+// template <class Real>
+// MVector<Real>& MVector<Real>::operator -= (const MVector& rhs)
+// {
+// 	assert(dimension == rhs.dimension);
+// 
+// 	for(int i=0; i<dimension; i++)
+// 		data[i] -= rhs.data[i];
+// 
+// 	return *this;
+// }
+
+
+// template <class Real>
+// MVector<Real>& MVector<Real>::operator += (const MVector& rhs)
+// {
+// 	assert(dimension == rhs.dimension);
+// 
+// 	for(int i=0; i<dimension; i++)
+// 		data[i] += rhs.data[i];
+// 
+// 	return *this;
+// }
+
+
+/*
+template <class Real>
+MVector<Real>& MVector<Real>::addFirstNElements(const MVector& rhs, int N)
+{
+	assert(N >= 0 && N < dimension);
+
+	for(int i=0; i<N; i++)
+		data[i] += rhs.data[i];
+
+	return *this;
+}
+*/
+/*
+template <class Real>
+MVector<Real> MVector<Real>::operator * (Real factor) const
+{
+	MVector newvec(dimension);
+
+	for(int i=0; i<dimension; i++)
+		newvec[i] = data[i] * factor;
+
+	return newvec;
+}
+*/
+
+
+template <class Real>
+MVector<Real>& MVector<Real>::operator *= (Real factor)
+{
+	for(int i=0; i<data.size(); i++)
+		data[i] *= factor;
+
+	return *this;
+}
+
+
+template <class Real>
+inline MVector<Real>& MVector<Real>::scale(Real factor)
+{
+	*this *= factor;
+
+	return *this;
+}
+
+
+/*
+template <class Real>
+MVector<Real> MVector<Real>::operator / (Real factor) const
+{
+	MVector newvec(dimension);
+
+	Real recip_factor = 1.0 / factor;
+
+	for(int i=0; i<dimension; i++)
+		newvec[i] = data[i] * recip_factor;
+
+	return newvec;
+}
+
+
+template <class Real>
+MVector<Real>& MVector<Real>::operator /= (Real factor)
+{
+	Real recip_factor = 1.0 / factor;
+
+	for(int i=0; i<dimension; i++)
+		data[i] *= recip_factor;
+
+	return *this;
+}
+
+
+template <class Real>
+MVector<Real>& MVector<Real>::normalise()
+{
+	*this /= length();
+
+	return *this;
+}
+
+template <class Real>
+Real MVector<Real>::length() const
+{
+	Real length = 0;
+
+	for(int i=0; i<dimension; i++)
+		length += data[i]*data[i];
+
+	return (Real)sqrt(length);
+}
+
+template <class Real>
+Real MVector<Real>::length2() const
+{
+	Real length2 = 0;
+
+	for(int i=0; i<dimension; i++)
+		length2 += data[i]*data[i];
+
+	return length2;
+}*/
+/*
+
+
+template <class Real>
+Real MVector<Real>::dotProduct(const MVector& rhs) const
+{
+	Real dot;
+
+	assert(dimension == rhs.dimension);
+
+	for(int i=0; i<dimension; i++)
+		dot += data[i] + rhs.data[i];
+
+	return dot;
+}
+
+template <class Real>
+Real MVector<Real>::angle(const MVector& rhs) const
+{
+	MVector thisnormized(*this);
+	thisnormized.normalise();
+
+	MVector rhsnormized(rhs);
+	rhsnormized.normalise();
+
+	return thisnormized.angleNormized(rhsnormized);
+}*/
+/*
+template <class Real>
+Real MVector<Real>::angleNormized(const MVector& rhs) const
+{
+	return acos( this->dotProduct(rhs) );
+}
+
+template <class Real>
+MVector<Real>& MVector<Real>::zero()
+{
+	for(int i=0; i<dimension; i++)
+		data[i] = 0;
+
+	return *this;
+}
+
+
+template <class Real>
+const Real* MVector<Real>::toReal() const
+{
+	return data;
+}
+
+template <class Real>
+Real* MVector<Real>::toReal()
+{
+	return data;
+}
+*/
+
+
+template <class Real>
+typename MVector<Real>::iterator MVector<Real>::begin() 
+{
+	return data.begin();
+}
+
+
+template <class Real>
+typename MVector<Real>::iterator MVector<Real>::end()
+{
+	return data.end();
+}
+
+
+template <class Real>
+typename MVector<Real>::const_iterator MVector<Real>::const_begin() const
+{
+	return data.begin();
+}
+
+
+template <class Real>
+typename MVector<Real>::const_iterator MVector<Real>::const_end() const
+{
+	return data.end();
+}
+
+
+/*
+template <class Real>
+Real MVector<Real>::sum() const
+{
+	Real s = 0.0;
+	for(int i=0; i<dimension; ++i)
+		s += data[i];
+	return s;
+}
+
+template <class Real>
+Real MVector<Real>::averageValue() const
+{
+	return sum() / (Real)dimension;
+}
+
+
+template <class Real>
+Real MVector<Real>::variance() const
+{
+	const Real expected = averageValue();
+	
+	Real diffsum = 0.0;
+	for(int i=0; i<dimension; ++i)
+		diffsum += (data[i] - expected) * (data[i] - expected);
+	
+	return diffsum / (Real)dimension;
+}
+*/
+/*
+template <class Real>
+Real MVector<Real>::Min() const
+{
+	assert(dimension > 0);
+	Real x = data[0];
+	for(int i=1; i<dimension; ++i)
+		x = myMin(x, data[i]);
+	return x;
+}
+
+
+template <class Real>
+Real MVector<Real>::Max() const
+{
+	assert(dimension > 0);
+	Real x = data[0];
+	for(int i=1; i<dimension; ++i)
+		x = myMax(x, data[i]);
+	return x;
+}
+
+*/
+
+
+#endif //__VECTOR_H__
