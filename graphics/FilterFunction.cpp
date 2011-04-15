f2a6170f0c9db56168d86bb5e500a4f40a9174ef
Thomas Ludwig <thomas.ludwig@gmail.com>
2011-04-15 16:51:24 +0000

    Filter functions can now have non-integer support Radial splatter can now have variable radius Radial splatter precomputing filter table
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@2022 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- graphics/FilterFunction.cpp
+++ graphics/FilterFunction.cpp
@@ -29,6 +29,7 @@ float* FilterFunction::getFilterData(int supersample_factor)
 	//assert(supersample_factor > 1);
 	//if(supersample_factor == cached_supersample_factor) return cached_filter_data;
 
+	const double filter_pixel_span_f = supportRadius() * 2 * supersample_factor;
 	const int filter_pixel_span  = (int)ceil(supportRadius() * 2) * supersample_factor;
 	const int filter_pixel_bound = filter_pixel_span / 2 - 1;
 
@@ -43,8 +44,8 @@ float* FilterFunction::getFilterData(int supersample_factor)
 
 		for(int u = -filter_pixel_bound; u <= filter_pixel_bound; ++u)
 		{
-			const double du = u / (double)(filter_pixel_span / 2);
-			const double dv = v / (double)(filter_pixel_span / 2);
+			const double du = u / (filter_pixel_span_f / 2);
+			const double dv = v / (filter_pixel_span_f / 2);
 			const double r  = sqrt(du * du + dv * dv);
 
 			partial_sum += eval(r);
@@ -57,8 +58,8 @@ float* FilterFunction::getFilterData(int supersample_factor)
 	for(int v = -filter_pixel_bound; v <= filter_pixel_bound; ++v)
 	for(int u = -filter_pixel_bound; u <= filter_pixel_bound; ++u)
 	{
-		const double du = u / (double)(filter_pixel_span / 2);
-		const double dv = v / (double)(filter_pixel_span / 2);
+		const double du = u / (filter_pixel_span_f / 2);
+		const double dv = v / (filter_pixel_span_f / 2);
 		const double r  = sqrt(du * du + dv * dv);
 
 		cached_filter_data[filter_addr++] = (float)(eval(r) / filter_sum);
