8eaaa1f5e2e74cae099dedc5e3e2b3d02ef5169d
Nicholas Chapman <nick@indigorenderer.com>
2012-02-02 12:28:00 +0000

    Setting SunSkyMaterial members to default values. Added test to RendererTests that uses a SunSkyMaterial background.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@3279 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- graphics/PNGDecoder.h
+++ graphics/PNGDecoder.h
@@ -40,6 +40,7 @@ public:
 	
 	static void decode(const std::string& path, Bitmap& bitmap_out);
 
+	// throws ImFormatExcep
 	static void write(const Bitmap& bitmap, const std::map<std::string, std::string>& metadata, const std::string& path);
 	static void write(const Bitmap& bitmap, const std::string& path); // Write with no metadata
 
