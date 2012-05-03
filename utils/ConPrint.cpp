46ca5a2f2633f9f450de3d735003dc5b1dc7fd71
Nicholas Chapman <nick@indigorenderer.com>
2012-05-03 13:52:43 +0000

    moved conPrint() etc.. out of IndigoDriver.cpp into ConPrint.cpp in utils. Cleanued up globals.h a bit.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@3690 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ utils/ConPrint.cpp
@@ -0,0 +1,44 @@
+/*=====================================================================
+ConPrint.cpp
+-------------------
+Copyright Glare Technologies Limited 2011 -
+Generated at 2012-05-03 12:20:21 +0100
+=====================================================================*/
+#include "ConPrint.h"
+
+
+#include "../utils/lock.h"
+#include "../utils/mutex.h"
+#include <iostream>
+
+
+// Use a mutex for conPrint etc.. to avoid intermingling of messages from multiple threads.
+static Mutex print_mutex;
+
+
+void conPrint(const std::string& s)
+{
+	Lock lock(print_mutex);
+
+	std::cout << s << std::endl;
+}
+
+
+void conPrintStr(const std::string& s)
+{
+	Lock lock(print_mutex);
+
+	std::cout << s;
+}
+
+
+void fatalError(const std::string& s)
+{
+	{
+	Lock lock(print_mutex);
+
+	std::cerr << "Fatal Error: " << s << std::endl;
+	}
+
+	exit(1);
+}
---
+++ utils/ConPrint.h
@@ -0,0 +1,23 @@
+/*=====================================================================
+ConPrint.h
+-------------------
+Copyright Glare Technologies Limited 2011 -
+Generated at 2012-05-03 12:20:21 +0100
+=====================================================================*/
+#pragma once
+
+
+#include <string>
+
+
+/*
+These functions should only be used for debugging and test code.
+*/
+#define printVar(v) conPrint(std::string(#v) + ": " + ::toString(v))
+
+void conPrint(const std::string& s);
+
+void conPrintStr(const std::string& s);
+
+void fatalError(const std::string& s);
+
