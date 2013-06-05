4041f8cf39ae52c3d323ba774b5f295d2d2e0b53
Nicholas Chapman <nick@indigorenderer.com>
2013-06-05 16:34:40 +0000

    Added checking for read failure in FileInStream. Worked around OS X bug by breaking large reads into chunks.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@5060 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- utils/FileInStream.cpp
+++ utils/FileInStream.cpp
@@ -9,6 +9,7 @@ Generated at 2013-01-28 15:26:30 +0000
 
 #include "fileutils.h"
 #include "Exception.h"
+#include "../maths/mathstypes.h"
 
 
 FileInStream::FileInStream(const std::string& path)
@@ -29,13 +30,39 @@ uint32 FileInStream::readUInt32()
 {
 	uint32 x;
 	file.read((char*)&x, sizeof(uint32));
+
+	if(file.fail())
+		throw Indigo::Exception("Read from file failed.");
+
 	return x;
 }
 
 
 void FileInStream::readData(void* buf, size_t num_bytes)
 {
-	 file.read((char*)buf, num_bytes);
+	// There's a bug on OS X that causes reads to fail when num_bytes >= 2^31 or so.
+	// Work around it by breaking the read into chunks.
+#ifdef OSX
+	char* tempbuf = (char*)buf;
+
+	while(num_bytes > 0)
+	{
+		const size_t num_to_read = myMin((size_t)1000000000u, num_bytes);
+		
+		file.read((char*)tempbuf, num_to_read); // Read chunk
+
+		if(file.fail())
+			throw Indigo::Exception("Read from file failed.");
+
+		tempbuf += num_to_read;
+		num_bytes -= num_to_read;
+	}
+#else
+	file.read((char*)buf, num_bytes);
+
+	if(file.fail())
+		throw Indigo::Exception("Read from file failed.");
+#endif
 }
 
 
