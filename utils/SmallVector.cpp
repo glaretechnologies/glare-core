d4f2ec70a0a7a619d03059d1cbe2abdee2fd6244
Nicholas Chapman <nick@indigorenderer.com>
2013-11-18 14:24:19 +0000

    Merged in arch glass branch. Changed step blend to false for automatically created null blend mats.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@5481 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ utils/SmallVector.cpp
@@ -0,0 +1,591 @@
+/*=====================================================================
+SmallSmallVector.cpp
+-------------------
+Copyright Glare Technologies Limited 2013 -
+Generated at 2013-03-07 14:27:12 +0000
+=====================================================================*/
+#include "SmallVector.h"
+
+
+#if BUILD_TESTS
+
+
+
+#include "stringutils.h"
+#include "timer.h"
+#include "../indigo/TestUtils.h"
+#include "../indigo/globals.h"
+#include <vector>
+#include <assert.h>
+
+
+class TestClass
+{};
+
+class TestCounterClass
+{
+public:
+	TestCounterClass(int& i_) : i(i_) { i++; }
+	TestCounterClass(const TestCounterClass& other) : i(other.i) { i++; }
+	~TestCounterClass() { i--; }
+
+	// std::vector use with GCC seems to require this.
+	//TestCounterClass& TestCounterClass::operator=(const TestCounterClass&)
+
+private:
+	int& i;
+};
+
+
+void SmallVectorTest()
+{
+	conPrint("SmallVectorTest()");
+
+	//========================= No-arg constructor =========================
+
+	// int type
+	{
+		const SmallVector<int, 4> v;
+		testAssert(v.size() == 0);
+	}
+
+
+	//========================= Count constructor =========================
+
+	// Count constructor with int type, with zero elems
+	{
+		const SmallVector<int, 4> v(
+			0, // count
+			123 // val
+		);
+		testAssert(v.size() == 0);
+	}
+
+	// Count constructor with int type, for various counts
+	{
+		for(size_t i=0; i<100; ++i)
+		{
+			const SmallVector<int, 4> v(
+				i, // count
+				123 // val
+			);
+			testAssert(v.size() == i);
+			for(size_t q=0; q<v.size(); ++q)
+				testAssert(v[q] == 123);
+		}
+	}
+
+	// Count constructor with int type
+	{
+		const SmallVector<int, 4> v(
+			10, // count
+			123 // val
+		);
+		testAssert(v.size() == 10);
+		for(size_t i=0; i<v.size(); ++i)
+			testAssert(v[i] == 123);
+	}
+
+	// Count constructor with TestClass
+	{
+		const SmallVector<TestClass, 4> v(
+			10 // count
+		);
+		testAssert(v.size() == 10);
+	}
+
+	// Count constructor with TestCounterClass
+	{
+		int ob_count = 0;
+		TestCounterClass dummy(ob_count);
+		testAssert(ob_count == 1);
+
+		{
+			const SmallVector<TestCounterClass, 4> v(
+				10, // count
+				dummy
+			);
+			testAssert(v.size() == 10);
+			testAssert(ob_count == 11);
+		}
+
+		testAssert(ob_count == 1);
+	}
+
+
+	//========================= Copy constructor =========================
+
+
+	// With int type, for various counts
+	{
+		for(size_t count=0; count < 100; ++count)
+		{
+			const SmallVector<int, 4> v(
+				count, // count
+				123 // val
+			);
+
+			// Copy construct v2 from v
+			const SmallVector<int, 4> v2(v);
+
+			testAssert(v2.size() == v.size());
+			for(size_t i=0; i<v2.size(); ++i)
+				testAssert(v2[i] == 123);
+		}
+	}
+
+	// With TestCounterClass
+	{
+		for(size_t count=0; count < 100; ++count)
+		{
+			int ob_count = 0;
+			TestCounterClass dummy(ob_count);
+			testAssert(ob_count == 1);
+
+			{
+				const SmallVector<TestCounterClass, 4> v(
+					count, // count
+					dummy
+				);
+				testAssert(v.size() == count);
+				testAssert(ob_count == count + 1);
+			
+				// Copy construct v2 from v
+				const SmallVector<TestCounterClass, 4> v2(v);
+
+				testAssert(v2.size() == v.size());
+				testAssert(ob_count == 2*count + 1);
+			}
+
+			testAssert(ob_count == 1);
+		}
+	}
+
+
+
+	//========================= operator = =========================
+
+	// Test assigning to self
+	{
+		SmallVector<int, 4> v(
+			10, // count
+			123 // val
+		);
+
+		v = v;
+
+		testAssert(v.size() == 10);
+		for(size_t i=0; i<v.size(); ++i)
+			testAssert(v[i] == 123);
+	}
+
+
+	// Test assigning from empty vector to empty vector
+	{
+		const SmallVector<int, 4> v;
+		SmallVector<int, 4> v2;
+		v2 = v;
+
+		testAssert(v.size() == 0);
+		testAssert(v2.size() == 0);
+	}
+
+
+	// With int type
+	{
+		const SmallVector<int, 4> v(
+			10, // count
+			123 // val
+		);
+
+		SmallVector<int, 4> v2;
+		v2 = v;
+
+		testAssert(v2.size() == 10);
+		for(size_t i=0; i<v2.size(); ++i)
+			testAssert(v2[i] == 123);
+	}
+
+	// With TestCounterClass, with existing capacity < other.size()
+	{
+		int ob_count = 0;
+		TestCounterClass dummy(ob_count);
+		testAssert(ob_count == 1);
+
+		{
+			const SmallVector<TestCounterClass, 4> v(
+				10, // count
+				dummy
+			);
+			testAssert(v.size() == 10);
+			testAssert(ob_count == 11);
+			
+			SmallVector<TestCounterClass, 4> v2;
+			v2 = v;
+
+			testAssert(v2.size() == 10);
+			testAssert(ob_count == 21);
+		}
+
+		testAssert(ob_count == 1);
+	}
+
+	// With TestCounterClass, with existing capacity >= other.size()
+	{
+		int ob_count = 0;
+		TestCounterClass dummy(ob_count);
+		testAssert(ob_count == 1);
+
+		{
+			const SmallVector<TestCounterClass, 4> v(
+				10, // count
+				dummy
+			);
+			testAssert(v.size() == 10);
+			testAssert(ob_count == 11);
+			
+			SmallVector<TestCounterClass, 4> v2;
+			v2.resize(100, dummy);
+			testAssert(v2.capacity() > 10);
+			v2 = v;
+
+			testAssert(v2.size() == 10);
+			testAssert(ob_count == 21);
+		}
+
+		testAssert(ob_count == 1);
+	}
+
+
+	//========================= reserve =========================
+	/*
+	
+	// With int type
+	{
+		SmallVector<int, 4> v(
+			10, // count
+			123 // val
+		);
+
+		testAssert(v.size() == 10);
+		testAssert(v.capacity() == 10);
+
+		v.reserve(100);
+
+		testAssert(v.size() == 10);
+		testAssert(v.capacity() == 100);
+	}
+
+
+	// With TestCounterClass
+	{
+		int ob_count = 0;
+		TestCounterClass dummy(ob_count);
+		testAssert(ob_count == 1);
+
+		{
+			SmallVector<TestCounterClass, 4> v(
+				10, // count
+				dummy
+			);
+			testAssert(v.size() == 10);
+			testAssert(ob_count == 11);
+			
+			v.reserve(100);
+
+			testAssert(v.size() == 10);
+			testAssert(v.capacity() == 100);
+			testAssert(ob_count == 11);
+
+			v.reserve(50);
+
+			testAssert(v.size() == 10);
+			testAssert(v.capacity() == 100);
+			testAssert(ob_count == 11);
+		}
+
+		testAssert(ob_count == 1);
+	}
+
+	*/
+	//========================= resize =========================
+
+	// With int type, zero size
+	{
+		SmallVector<int, 4> v;
+		v.resize(0);
+		testAssert(v.size() == 0);
+	}
+
+
+	// With int type
+	{
+		SmallVector<int, 4> v(
+			10, // count
+			123 // val
+		);
+
+		testAssert(v.size() == 10);
+		testAssert(v.capacity() == 10);
+
+		v.resize(100);
+
+		testAssert(v.size() == 100);
+		testAssert(v.capacity() >= 100);
+	}
+
+
+	// With TestCounterClass
+	{
+		int ob_count = 0;
+		TestCounterClass dummy(ob_count);
+		testAssert(ob_count == 1);
+
+		{
+			SmallVector<TestCounterClass, 4> v(
+				10, // count
+				dummy
+			);
+			testAssert(v.size() == 10);
+			testAssert(ob_count == 11);
+			
+			// Resize a lot larger to cause capacity expansion
+			v.resize(100, dummy);
+
+			testAssert(v.size() == 100);
+			testAssert(v.capacity() >= 100);
+			testAssert(ob_count == 101);
+
+			// Resize just a little bit larger to avoid capacity expansion
+			//v.reserve(101);
+			v.resize(101, dummy);
+
+			testAssert(v.size() == 101);
+			testAssert(v.capacity() >= 101);
+			testAssert(ob_count == 102);
+
+			v.resize(20, dummy);
+
+			testAssert(v.size() == 20);
+			testAssert(v.capacity() >= 100);
+			testAssert(ob_count == 21);
+
+			// Try resizing to same size
+			v.resize(20, dummy);
+
+			testAssert(v.size() == 20);
+			testAssert(v.capacity() >= 100);
+			testAssert(ob_count == 21);
+
+			// Try resizing to zero.
+			v.resize(0, dummy);
+
+			testAssert(v.size() == 0);
+			//testAssert(v.capacity() >= 100);
+			testAssert(ob_count == 1);
+		}
+
+		testAssert(ob_count == 1);
+	}
+
+	// With TestCounterClass
+	{
+		for(size_t start_size=0; start_size<10; ++start_size)
+		for(size_t end_size=0; end_size<100; ++end_size)
+		{
+			int ob_count = 0;
+			TestCounterClass dummy(ob_count);
+			testAssert(ob_count == 1);
+
+			{
+				SmallVector<TestCounterClass, 4> v(
+					start_size, // count
+					dummy
+				);
+				testAssert(v.size() == start_size);
+				testAssert(ob_count == start_size + 1);
+			
+				// Resize to end_size
+				v.resize(end_size, dummy);
+
+				testAssert(v.size() == end_size);
+				//testAssert(v.capacity() >= 100);
+				testAssert(ob_count == end_size + 1);
+
+			}
+
+			testAssert(ob_count == 1);
+		}
+	}
+
+
+	//========================= push_back =========================
+
+	// With int type
+	{
+		SmallVector<int, 4> v;
+		v.push_back(1);
+		testAssert(v.size() == 1);
+		testAssert(v[0] == 1);
+
+		v.push_back(2);
+		v.push_back(3);
+		v.push_back(4);
+
+		testAssert(v.size() == 4);
+
+		for(int i=0; i<4; ++i)
+			testAssert(v[i] == i + 1);
+	}
+
+	// With int type, lots of push backs
+	{
+		SmallVector<int, 4> v;
+		const size_t N = 10000;
+		for(size_t i=0; i<N; ++i)
+		{
+			v.push_back((int)i);
+			testAssert(v.size() == i + 1);
+			testAssert(v.capacity() >= i + 1);
+		}
+
+		testAssert(v.size() == N);
+		testAssert(v.capacity() >= N);
+
+		for(int i=0; i<N; ++i)
+			testAssert(v[i] == (int)i);
+	}
+
+
+
+	// With TestCounterClass
+	{
+		int ob_count = 0;
+		TestCounterClass dummy(ob_count);
+		testAssert(ob_count == 1);
+
+		{
+			SmallVector<TestCounterClass, 4> v;
+
+			const size_t N = 10000;
+			for(size_t i=0; i<N; ++i)
+			{
+				v.push_back(dummy);
+
+				testAssert(v.size() == i + 1);
+				testAssert(ob_count == v.size() + 1);
+			}
+		}
+
+		testAssert(ob_count == 1);
+	}
+
+	//========================= back =========================
+
+
+	// With int type, lots of push backs
+	{
+		SmallVector<int, 4> v;
+		size_t N = 10000;
+		for(size_t i=0; i<N; ++i)
+		{
+			v.push_back((int)i);
+
+			testAssert(v.back() == (int)i);
+		}
+
+		testAssert(v.size() == N);
+		testAssert(v.capacity() >= N);
+
+		for(int i=0; i<N; ++i)
+			testAssert(v[i] == (int)i);
+	}
+
+	//========================= operator [] =========================
+
+
+	// With int type, lots of push backs
+	{
+		SmallVector<int, 4> v;
+		size_t N = 100;
+		for(size_t i=0; i<N; ++i)
+		{
+			v.push_back((int)i);
+
+			testAssert(v.back() == (int)i);
+
+			for(size_t q=0; q<v.size(); ++q)
+				testAssert(v[q] == (int)q);
+		}
+
+		testAssert(v.size() == N);
+		testAssert(v.capacity() >= N);
+
+		for(int i=0; i<N; ++i)
+			testAssert(v[i] == (int)i);
+	}
+
+
+
+
+	//========================= pop_back =========================
+
+	// With int type
+	{
+		SmallVector<int, 4> v;
+		const size_t N = 100;
+		for(size_t i=0; i<N; ++i)
+			v.push_back((int)i);
+
+		for(size_t i=0; i<N; ++i)
+		{
+			v.pop_back();
+
+			testAssert(v.size() == N - (i + 1));
+
+			// Check vector elements
+			for(size_t q=0; q<v.size(); ++q)
+			{
+				testAssert(v[q] == (int)q);
+			}
+		}
+	}
+
+
+
+	// With TestCounterClass
+	{
+		const size_t N = 100;
+
+		int ob_count = 0;
+		TestCounterClass dummy(ob_count);
+		testAssert(ob_count == 1);
+
+		{
+			SmallVector<TestCounterClass, 4> v;
+			
+			// Push back N times
+			for(size_t i=0; i<N; ++i)
+				v.push_back(dummy);
+
+			testAssert(ob_count == 1 + v.size());
+			
+			// Pop back N times
+			for(size_t i=0; i<N; ++i)
+			{
+				v.pop_back();
+
+				testAssert(v.size() == N - (i + 1));
+				testAssert(ob_count == 1 + v.size());
+			}
+		}
+
+		testAssert(ob_count == 1);
+	}
+
+
+
+}
+
+
+#endif // BUILD_TESTS
---
+++ utils/SmallVector.h
@@ -0,0 +1,636 @@
+/*=====================================================================
+SmallVector.h
+-------------------
+Copyright Glare Technologies Limited 2013 -
+Generated at 2013-03-07 14:27:12 +0000
+=====================================================================*/
+#pragma once
+
+
+#include "../maths/SSE.h"
+#include "../maths/mathstypes.h"
+#include <assert.h>
+#include <memory>
+#include <new>
+
+
+/*=====================================================================
+SmallVector
+-------------------
+Like js::Vector but does the small vector optimisation - 
+if the number of elements stored is small (<= N), then store them directly in the object
+as opposed to somewhere else in the heap.
+=====================================================================*/
+template <class T, int N>
+class SmallVector
+{
+public:
+	inline SmallVector(); // Initialise as an empty vector.
+	inline SmallVector(size_t count, const T& val = T()); // Initialise with count copies of val.
+	inline SmallVector(const SmallVector& other); // Initialise as a copy of other
+	inline ~SmallVector();
+
+	inline SmallVector& operator=(const SmallVector& other);
+
+	inline void reserve(size_t N); // Make sure capacity is at least N.
+	inline void resize(size_t N, const T& val = T()); // Resize to size N, using copies of val if N > size().
+	inline size_t capacity() const;
+	inline size_t size() const;
+	inline bool empty() const;
+
+	inline void push_back(const T& t);
+	inline void pop_back();
+	inline const T& back() const;
+	inline T& back();
+	inline T& operator[](size_t index);
+	inline const T& operator[](size_t index) const;
+
+	typedef T* iterator;
+	typedef const T* const_iterator;
+
+	inline iterator begin();
+	inline iterator end();
+	inline const_iterator begin() const;
+	inline const_iterator end() const;
+	
+private:
+	inline void invariant() const;
+	inline void destroyExistingDirectObjects();
+	inline void destroyExistingHeapObjectsAndFreeMem();
+
+	struct SmallVectorHeap
+	{
+		T* e; // Elements
+		size_t capacity_; // This is the number of elements we have roon for in e.
+	};
+
+	struct SmallVectorDirect
+	{
+		//T e[N];
+		unsigned char e[sizeof(T) * N];
+	};
+
+	union SmallVectorUnion
+	{
+		SmallVectorHeap heap;
+		SmallVectorDirect direct;
+	};
+
+	SmallVectorUnion data;
+
+	size_t size_; // Number of elements in the vector.  Elements e[0] to e[size_-1] are proper constructed objects.
+};
+
+
+void SmallVectorTest();
+
+
+template <class T, int N>
+SmallVector<T, N>::SmallVector()
+:	size_(0)
+{
+	invariant();
+}
+
+
+
+template <class T, int N>
+SmallVector<T, N>::SmallVector(size_t count, const T& val)
+:	size_(0)
+{
+	invariant();
+	resize(count, val);
+	invariant();
+}
+
+
+template <class T, int N>
+SmallVector<T, N>::SmallVector(const SmallVector<T, N>& other)
+{
+	if(other.size_ <= N)
+	{
+		// Copy-construct new objects from existing objects in 'other'.
+		std::uninitialized_copy(reinterpret_cast<const T*>(other.data.direct.e), reinterpret_cast<const T*>(other.data.direct.e) + other.size_, 
+			reinterpret_cast<T*>(data.direct.e) // dest
+		);
+
+		size_ = other.size_;
+	}
+	else
+	{
+		// Allocate new memory
+		data.heap.e = static_cast<T*>(SSE::alignedSSEMalloc(sizeof(T) * other.size_));
+
+		// Copy-construct new objects from existing objects in 'other'.
+		std::uninitialized_copy(other.data.heap.e, other.data.heap.e + other.size_, 
+			data.heap.e // dest
+		);
+		
+		size_ = other.size_;
+		data.heap.capacity_ = other.size_;
+	}
+
+	invariant();
+}
+
+
+template <class T, int N>
+SmallVector<T, N>::~SmallVector()
+{
+	invariant();
+
+	// Destroy objects
+	if(size_ <= N)
+		destroyExistingDirectObjects();
+	else
+		destroyExistingHeapObjectsAndFreeMem();
+}
+
+
+template <class T, int N>
+SmallVector<T, N>& SmallVector<T, N>::operator=(const SmallVector& other)
+{
+	invariant();
+
+	if(this == &other)
+		return *this;
+
+	if(other.size_ <= N)
+	{
+		if(size_ <= N)
+		{
+			// If current vector is direct storing:
+			destroyExistingDirectObjects();
+		}
+		else
+		{
+			// Else if current vector is heap storing:
+			destroyExistingHeapObjectsAndFreeMem();
+		}
+
+		// Copy elements over from other
+		if(other.size_ > 0) 
+			std::uninitialized_copy(reinterpret_cast<const T*>(other.data.direct.e), reinterpret_cast<const T*>(other.data.direct.e) + other.size_, 
+				reinterpret_cast<T*>(data.direct.e)
+			);
+
+		size_ = other.size_;
+	}
+	else
+	{
+		// Else we will need to store new elements on the heap:
+
+		if(size_ <= N) // If current vector is direct storing:
+		{
+			destroyExistingDirectObjects();
+
+			// Allocate new memory
+			data.heap.e = static_cast<T*>(SSE::alignedSSEMalloc(sizeof(T) * other.size_));
+
+			// Copy elements over from other
+			std::uninitialized_copy(other.data.heap.e, other.data.heap.e + other.size_, data.heap.e);
+
+			size_ = other.size_;
+			data.heap.capacity_ = other.size_;
+		}
+		else
+		{
+			/*
+			Else if current vector is already heap storing:
+			
+			if we already have the capacity that we need, just copy objects over.
+
+			otherwise free existing mem, alloc mem, copy elems over
+			*/
+
+			if(data.heap.capacity_ >= other.size_)
+			{
+				// Destroy existing objects
+				for(size_t i=0; i<size_; ++i)
+					data.heap.e[i].~T();
+
+				// Copy elements over from other
+				//if(e) 
+					std::uninitialized_copy(other.data.heap.e, other.data.heap.e + other.size_, data.heap.e);
+
+				size_ = other.size_;
+			}
+			else
+			{
+				// Destroy existing objects
+				for(size_t i=0; i<size_; ++i)
+					data.heap.e[i].~T();
+
+				// Free existing mem
+				SSE::alignedFree(data.heap.e);
+
+				// Allocate new memory
+				data.heap.e = static_cast<T*>(SSE::alignedSSEMalloc(sizeof(T) * other.size_));
+
+				// Copy elements over from other
+				std::uninitialized_copy(other.data.heap.e, other.data.heap.e + other.size_, data.heap.e);
+
+				size_ = other.size_;
+				data.heap.capacity_ = other.size_;
+			}
+		}
+	}
+
+	assert(size() == other.size());
+
+
+	invariant();
+
+	return *this;
+}
+
+
+template <class T, int N>
+void SmallVector<T, N>::reserve(size_t n)
+{
+	invariant();
+
+	// Capacity is always >= N.
+
+	if(size_ <= N)
+	{
+		// We can't expand capacity when storing directly, capacity is always N.
+	}
+	else // Else if storing on heap:
+	{
+		if(n > data.heap.capacity_) // If need to expand capacity
+		{
+			// Allocate new memory
+			T* new_e = static_cast<T*>(SSE::alignedSSEMalloc(sizeof(T) * n));
+
+			// Copy-construct new objects from existing objects.
+			// e[0] to e[size_-1] will now be proper initialised objects.
+			std::uninitialized_copy(data.heap.e, data.heap.e + size_, new_e);
+		
+			if(data.heap.e)
+			{
+				// Destroy old objects
+				for(size_t i=0; i<size_; ++i)
+					data.heap.e[i].~T();
+
+				SSE::alignedFree(data.heap.e); // Free old buffer.
+			}
+
+			data.heap.e = new_e;
+			data.heap.capacity_ = n;
+		}
+	}
+
+	invariant();
+}
+
+
+template <class T, int N>
+void SmallVector<T, N>::resize(size_t new_size, const T& val)
+{
+	invariant();
+
+	if(new_size > size_) // If we are increasing the size:
+	{
+		if(new_size <= N) // If the new size still fits in direct storage:
+		{
+			// Initialise elements e[size_] to e[new_size-1] as a copy of val.
+			std::uninitialized_fill(reinterpret_cast<T*>(data.direct.e) + size_, reinterpret_cast<T*>(data.direct.e) + new_size, val);
+		}
+		else
+		{
+			// Else elements need to go on heap now
+
+			if(size_ <= N)
+			{
+				// If elements are current direct stored:
+
+				// Allocate new memory
+				const size_t newcapacity = new_size;
+				T* new_e = static_cast<T*>(SSE::alignedSSEMalloc(sizeof(T) * newcapacity));
+
+				// Copy existing elements over to new memory
+				std::uninitialized_copy(reinterpret_cast<const T*>(data.direct.e), reinterpret_cast<const T*>(data.direct.e) + size_, 
+					new_e);
+
+				// Initialise elements e[size_] to e[new_size-1] as a copy of val.
+				std::uninitialized_fill(new_e + size_, new_e + new_size, val);
+
+				// Destroy old direct-stored elements
+				for(size_t i=0; i<size_; ++i)
+					(reinterpret_cast<T*>(data.direct.e) + i)->~T();
+
+				// Switch to storing on heap
+				data.heap.e = new_e;
+				data.heap.capacity_ = newcapacity;
+			}
+			else
+			{
+				// Else if elements are current heap stored:
+
+				if(new_size > data.heap.capacity_)
+				{
+					const size_t newcapacity = myMax(new_size, data.heap.capacity_ + data.heap.capacity_ / 2); // current size * 1.5
+					reserve(newcapacity);
+				}
+
+				// Initialise elements e[size_] to e[n-1] as a copy of val.
+				std::uninitialized_fill(data.heap.e + size_, data.heap.e + new_size, val);
+			}
+		}
+
+	}
+	else if(new_size < size_) // Else if we are decreasing the size:
+	{
+		if(size_ <= N) // If we are storing directly
+		{
+			// Destroy elements e[new_size] to e[size-1]
+			for(size_t i=new_size; i<size_; ++i)
+				(reinterpret_cast<T*>(data.direct.e) + i)->~T();
+		}
+		else // Else if we are currently storing on heap
+		{
+			if(new_size > N)
+			{
+				// We keep using the heap, and keep the same mem, just destroy some elems
+				for(size_t i=new_size; i<size_; ++i)
+					data.heap.e[i].~T();
+			}
+			else
+			{
+				// We need to change from using the heap to storing directly.
+
+				// NOTE: This way is slow, we should move elements that we are keeping directly instead of constructing new ones and destroying old ones.
+
+				// Copy elements we are keeping (e[0] to e[new_size-1] from the heap to direct storage
+				T* heap_elems = data.heap.e;
+				std::uninitialized_copy(heap_elems, heap_elems + new_size, 
+					reinterpret_cast<T*>(data.direct.e)
+				);
+
+				// Destroy elements e[0] to e[size_]
+				for(size_t i=0; i<size_; ++i)
+					heap_elems[i].~T();
+
+				// Free heap memory
+				SSE::alignedFree(heap_elems);
+			}
+		}
+			
+	}
+
+	size_ = new_size;
+
+	invariant();
+}
+
+
+template <class T, int N>
+size_t SmallVector<T, N>::capacity() const
+{
+	invariant();
+
+	if(size_ <= N)
+		return N;
+	else
+		return data.heap.capacity_;
+}
+
+
+template <class T, int N>
+size_t SmallVector<T, N>::size() const
+{
+	invariant();
+	return size_;
+}
+
+
+template <class T, int N>
+bool SmallVector<T, N>::empty() const
+{
+	invariant();
+	return size_ == 0;
+}
+
+
+template <class T, int N>
+void SmallVector<T, N>::push_back(const T& t)
+{
+	invariant();
+
+	if(size_ <= N) // if we are storing directly
+	{
+		if(size_ + 1 <= N) // If we have direct storage room for the new element
+		{
+			// Construct e[size_] from t
+			::new (reinterpret_cast<T*>(data.direct.e) + size_) T(t);
+			size_++;
+		}
+		else
+		{
+			resize(size_ + 1, t);
+		}
+	}
+	else
+	{
+		if(size_ + 1 > data.heap.capacity_)
+		{
+			const size_t newcapacity = myMax(size_ + 1, data.heap.capacity_ + data.heap.capacity_ / 2); // current size * 1.5
+			reserve(newcapacity);
+		}
+
+		// Construct e[size_] from t
+		::new (data.heap.e + size_) T(t);
+		size_++;
+	}
+	
+
+	invariant();
+}
+
+
+template <class T, int N>
+void SmallVector<T, N>::pop_back()
+{
+	invariant();
+
+	if(size_ <= N) // If we are storing directly
+	{
+		// Destroy element e[size_ - 1]
+		(reinterpret_cast<T*>(data.direct.e) + size_ - 1)->~T();
+	}
+	else // Else if we are currently storing on heap
+	{
+		if(size_ - 1 > N) // if size > N + 1
+		{
+			// We keep using the heap, and keep the same mem, just destroy element e[size_ - 1]
+			data.heap.e[size_ - 1].~T();
+		}
+		else // else size_ == N + 1
+		{
+			assert(size_ == N + 1);
+
+			// We need to change from using the heap to storing directly.
+
+			// NOTE: This way is slow, we should move elements that we are keeping directly instead of constructing new ones and destroying old ones.
+
+			// Copy elements we are keeping (e[0] to e[N]) from the heap to direct storage
+			T* heap_elems = data.heap.e;
+			std::uninitialized_copy(heap_elems, heap_elems + N, 
+				reinterpret_cast<T*>(data.direct.e)
+			);
+
+			// Destroy elements e[0] to e[size_]
+			for(size_t i=0; i<size_; ++i)
+				heap_elems[i].~T();
+
+			// Free heap memory
+			SSE::alignedFree(heap_elems);
+		}
+	}
+
+	size_--;
+
+	invariant();
+}
+
+
+template <class T, int N>
+const T& SmallVector<T, N>::back() const
+{
+	invariant();
+	assert(size_ >= 1);
+
+	if(size_ <= N)
+		return *(reinterpret_cast<const T*>(data.direct.e) + (size_ - 1));
+	else
+		return data.heap.e[size_ - 1];
+}
+
+
+template <class T, int N>
+T& SmallVector<T, N>::back()
+{
+	invariant();
+	assert(size_ >= 1);
+
+	if(size_ <= N)
+		return *(reinterpret_cast<T*>(data.direct.e) + (size_ - 1));
+	else
+		return data.heap.e[size_ - 1];
+}
+
+
+template <class T, int N>
+T& SmallVector<T, N>::operator[](size_t index)
+{
+	invariant();
+	assert(index < size_);
+
+	if(size_ <= N)
+		return *(reinterpret_cast<T*>(data.direct.e) + index);
+	else
+		return data.heap.e[index];
+}
+
+
+template <class T, int N>
+const T& SmallVector<T, N>::operator[](size_t index) const
+{
+	invariant();
+	assert(index < size_);
+
+	if(size_ <= N)
+		return *(reinterpret_cast<const T*>(data.direct.e) + index);
+	else
+		return data.heap.e[index];
+}
+
+
+template <class T, int N>
+void SmallVector<T, N>::invariant() const
+{
+#ifndef NDEBUG
+	if(size_ < N)
+	{
+		// Elements are directly stored in class.
+		// Nothing to check
+	}
+	else
+	{
+		assert(data.heap.capacity_ >= size_);
+
+		if(size_ > 0)
+		{
+			assert(data.heap.e != NULL);
+		}
+		else
+		{
+			assert(data.heap.e == NULL);
+		}
+	}
+
+#endif
+
+}
+
+
+template <class T, int N>
+void SmallVector<T, N>::destroyExistingDirectObjects()
+{
+	assert(size_ <= N);
+	for(size_t i=0; i<size_; ++i)
+		(reinterpret_cast<const T*>(data.direct.e) + i)->~T();
+}
+
+
+template <class T, int N>
+void SmallVector<T, N>::destroyExistingHeapObjectsAndFreeMem()
+{
+	assert(size_ > N);
+	
+	for(size_t i=0; i<size_; ++i)
+		data.heap.e[i].~T();
+
+	SSE::alignedFree(data.heap.e);
+}
+
+
+template <class T, int N>
+typename SmallVector<T, N>::iterator SmallVector<T, N>::begin()
+{
+	if(size_ <= N)
+		return reinterpret_cast<T*>(data.direct.e);
+	else
+		return data.heap.e;
+}
+
+
+template <class T, int N>
+typename SmallVector<T, N>::iterator SmallVector<T, N>::end()
+{
+	if(size_ <= N)
+		return reinterpret_cast<T*>(data.direct.e) + size_;
+	else
+		return data.heap.e + size_;
+}
+
+
+template <class T, int N>
+typename SmallVector<T, N>::const_iterator SmallVector<T, N>::begin() const
+{
+	if(size_ <= N)
+		return reinterpret_cast<const T*>(data.direct.e);
+	else
+		return data.heap.e;
+}
+
+
+template <class T, int N>
+typename SmallVector<T, N>::const_iterator SmallVector<T, N>::end() const
+{
+	if(size_ <= N)
+		return reinterpret_cast<const T*>(data.direct.e) + size_;
+	else
+		return data.heap.e + size_;
+}
