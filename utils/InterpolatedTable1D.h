55f5923469a07a4c6f3a4bfe3bdbd286c821c306
Nicholas Chapman <nick@indigorenderer.com>
2013-12-06 14:49:33 +0000

    Added replacement of perezFuncs with lookup tables in SkyLight (old sky model).  Needs more testing. Optimised InterpolatedTable1D.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@5589 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- utils/InterpolatedTable1D.h
+++ utils/InterpolatedTable1D.h
@@ -14,7 +14,7 @@ Generated at Thu Oct 07 22:28:38 +1300 2010
 /*=====================================================================
 InterpolatedTable1D
 -------------------
-
+data.size() must be < 2^31
 =====================================================================*/
 template <class Real, class Datum>
 class InterpolatedTable1D
@@ -32,6 +32,7 @@ public:
 
 private:
 	std::vector<Datum> data;
+	int data_size;
 	Real start_x;
 	Real end_x;
 	Real gap_width;
@@ -47,7 +48,8 @@ InterpolatedTable1D<Real, Datum>::InterpolatedTable1D(
 )
 :	data(data_),
 	start_x(start_x_),
-	end_x(end_x_)
+	end_x(end_x_),
+	data_size((int)data_.size())
 {
 	assert(data.size() >= 2);
 	assert(end_x > start_x);
@@ -59,20 +61,16 @@ InterpolatedTable1D<Real, Datum>::InterpolatedTable1D(
 template <class Real, class Datum>
 Datum InterpolatedTable1D<Real, Datum>::getValue(Real x) const
 {
-	if(x < start_x)
-		return data[0];
-
-	const int index = (int)((x - start_x) * recip_gap_width);
-	//const int index = Maths::floorToInt((x - start_x) * recip_gap_width);
+	const float offset = (x - start_x) * recip_gap_width;
+	const int index = (int)offset;
 
 	if(index < 0)
 		return data[0];
-	if(index + 1 >= (int)data.size())
-		return data[data.size() - 1];
+	if(index + 1 >= data_size)
+		return data[data_size - 1];
 
-	const Real index_x = start_x + (Real)index * gap_width;
-
-	const Real t = (x - index_x) * recip_gap_width;
+	const float t = offset - index;
+	assert(t >= 0 && t <= 1);
 
 	return Maths::uncheckedLerp(data[index], data[index+1], t);
 }
