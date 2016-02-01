2397875b2015e9ab434ae1dcfe24588baacccffc
Nicholas Chapman <nick@indigorenderer.com>
2016-02-01 00:26:14 +0000

    Cleaned up OpenSSL.cpp a bit.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@7585 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- utils/OpenSSL.cpp
+++ utils/OpenSSL.cpp
@@ -7,15 +7,9 @@ Copyright Glare Technologies Limited 2016 -
 
 
 #include "Mutex.h"
-#include <openssl/rsa.h>
-#include <openssl/evp.h>
-#include <openssl/bio.h>
-#include <openssl/objects.h>
-#include <openssl/x509.h>
-#include <openssl/err.h>
-#include <openssl/pem.h>
 #include <openssl/crypto.h>
 #include <vector>
+#include <cassert>
 
 
 /*
@@ -25,13 +19,14 @@ https://www.openssl.org/docs/manmaster/crypto/threads.html
 */
 
 
-//static std::vector<Mutex*> mutexes;
 static Mutex* mutexes = NULL;
 static int num_mutexes = 0;
 
 
 static void locking_callback(int mode, int type, const char* file, int line)
 {
+	assert(type >= 0 && type < num_mutexes);
+
 	if(mode & CRYPTO_LOCK)
 		mutexes[type].acquire();
 	else
@@ -39,7 +34,7 @@ static void locking_callback(int mode, int type, const char* file, int line)
 }
 
 
-unsigned long threadid_func_callback()
+static unsigned long threadid_func_callback()
 {
 #ifdef _WIN32
 	return ::GetCurrentThreadId();
@@ -51,18 +46,13 @@ unsigned long threadid_func_callback()
 
 void OpenSSL::init()
 {
-	//if(!mutexes.empty()) // If init has already been called:
-	//	return;
-	if(mutexes)
+	if(mutexes) // If init has already been called:
 		return;
 
-	//mutexes.resize(CRYPTO_num_locks());
 	num_mutexes = CRYPTO_num_locks();
 	mutexes = (Mutex*)malloc(sizeof(Mutex) * num_mutexes);
 	for(size_t i=0; i<num_mutexes; ++i)
 		new (&mutexes[i]) Mutex(); // Construct it
-	//for(size_t i=0; i<mutexes.size(); ++i)
-	//	mutexes[i] = new Mutex();
 
 	CRYPTO_set_locking_callback(locking_callback);
 
@@ -78,8 +68,6 @@ void OpenSSL::shutdown()
 
 	for(size_t i=0; i<num_mutexes; ++i)
 		mutexes[i].~Mutex();
-		//delete mutexes[i];
-	//mutexes.clear();
 
 	free(mutexes);
 }
