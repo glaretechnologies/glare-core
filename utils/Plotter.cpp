71234ed4267439db69cb2d96884dd1d9606a9d97
Nicholas Chapman <nick@indigorenderer.com>
2016-05-04 12:20:55 +0000

    Some improvements to Plotter. Addedx z-range for plot3D().
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@7937 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- utils/Plotter.cpp
+++ utils/Plotter.cpp
@@ -36,75 +36,101 @@ void Plotter::plot(
 		PlotOptions options
 		)
 {
-	const std::string temp_dir = StringUtils::replaceCharacter(PlatformUtils::getTempDirPath(), '\\', '/'); // Gnuplot needs forwards slashes
-	const std::string temp_data_path = temp_dir + "/plotdata.txt";
-
-	// Write data
-	for(unsigned int i=0; i<data.size(); ++i)
+	try
 	{
-		std::ofstream f(FileUtils::convertUTF8ToFStreamPath(temp_data_path + ::toString(i) + ".txt").c_str());
+		const std::string temp_dir = StringUtils::replaceCharacter(PlatformUtils::getTempDirPath(), '\\', '/'); // Gnuplot needs forwards slashes
+		const std::string temp_data_path = temp_dir + "/plotdata";
 
-		for(size_t z=0; z<data[i].points.size(); ++z)
-			f << data[i].points[z].x << " " << data[i].points[z].y << "\n";
-	}
-
-
-	// Write Gnuplot control script
-	const std::string temp_path = temp_dir + "/plot.txt";
-
-	{
-		std::ofstream f(FileUtils::convertUTF8ToFStreamPath(temp_path).c_str());
-
-		// NOTE: old terminal type was 'png'
-
-		f << "set terminal pngcairo size " + toString(options.w) + "," + toString(options.h) + "\n";
-		f << "set output \"" + path + "\"\n";
-		if(options.polar)
-		{
-			float r_max = 0.5f;
-			f << "set polar\n";
-			f << "set grid polar\n";
-			f << "set view equal xy\n";
-			f << "set size ratio 0.5\n";
-			f << "set xrange [-" << r_max << ":" << r_max << "]\n";
-			f << "set yrange [0:" << r_max << "]\n";
-			//f << "set xrange [-10:10]\n";
-			//f << "set yrange [0:10]\n";
-			f << "set xtics 0.5\n";
-			//if(options.r_log)
-			//	f << "set logscale r 10\n";
-
-		}
+		// Write data
 		
-		if(options.explicit_x_range)
-		{
-			f << "set xrange [" << options.x_start << ":" << options.x_end << "]\n";
-		}
-
-		f << "set style data lines\n";
-		//f << "set grid linetype rgb \"blue\"  lw 0.4\n";
-		//f << "set style data linespoints\n";
-		if(!options.polar)
-		{
-			f << "set xlabel \"" + x_label + "\"\n";
-			f << "set ylabel \"" + y_label + "\"\n";
-		}
-		f << "set title \"" + title + "\"\n";
-		//f << "set encoding iso_8859_1\n"
-		if(options.x_axis_log)
-			f << "set logscale x\n";
-		if(options.y_axis_log)
-			f << "set logscale y\n";
-		f << "plot ";
 		for(unsigned int i=0; i<data.size(); ++i)
-			f << "\"" + temp_data_path + ::toString(i) + ".txt" + "\" using 1:2 title \"" + data[i].key + "\" " + (i < data.size() - 1 ? "," : "");
-		f << "\n";
+		{
+			std::string data_s;
+			for(size_t z=0; z<data[i].points.size(); ++z)
+				data_s += toString(data[i].points[z].x) + " " + toString(data[i].points[z].y) + "\n";
+
+			FileUtils::writeEntireFileTextMode(temp_data_path + ::toString(i) + ".txt", data_s);
+		}
+
+
+		// Write Gnuplot control script
+		const std::string temp_path = temp_dir + "/plot.txt";
+
+		{
+			std::ofstream f(FileUtils::convertUTF8ToFStreamPath(temp_path).c_str());
+
+			// NOTE: old terminal type was 'png'
+
+			f << "set terminal pngcairo size " + toString(options.w) + "," + toString(options.h) + "\n";
+			f << "set output \"" + path + "\"\n";
+			if(options.polar)
+			{
+				float r_max = 0.5f;
+				f << "set polar\n";
+				f << "set grid polar\n";
+				f << "set view equal xy\n";
+				f << "set size ratio 0.5\n";
+				f << "set xrange [-" << r_max << ":" << r_max << "]\n";
+				f << "set yrange [0:" << r_max << "]\n";
+				//f << "set xrange [-10:10]\n";
+				//f << "set yrange [0:10]\n";
+				f << "set xtics 0.5\n";
+				//if(options.r_log)
+				//	f << "set logscale r 10\n";
+			}
+
+			// Draw vertical lines (see http://stackoverflow.com/questions/4457046/how-do-i-draw-a-vertical-line-in-gnuplot)
+			for(size_t i=0; i<options.vertical_lines.size(); ++i)
+				f << "set arrow from " + toString(options.vertical_lines[i]) + " ,0 to " + toString(options.vertical_lines[i]) + ",1 nohead lc rgb 'green' \n";
+
+			// Draw horizontal lines
+			for(size_t i=0; i<options.horizontal_lines.size(); ++i)
+				f << "set arrow from 0," + toString(options.horizontal_lines[i]) + " to 1," + toString(options.horizontal_lines[i]) + " nohead lc rgb 'green' \n";
+
+
+			f << "set grid x y\n";
+		
+			if(options.explicit_x_range)
+				f << "set xrange [" << options.x_start << ":" << options.x_end << "]\n";
+			if(options.explicit_y_range)
+				f << "set yrange [" << options.y_start << ":" << options.y_end << "]\n";
+
+			f << "set style data lines\n";
+			//f << "set grid linetype rgb \"blue\"  lw 0.4\n";
+			//f << "set style data linespoints\n";
+			if(!options.polar)
+			{
+				f << "set xlabel \"" + x_label + "\"\n";
+				f << "set ylabel \"" + y_label + "\"\n";
+			}
+			f << "set title \"" + title + "\"\n";
+			//f << "set encoding iso_8859_1\n"
+			if(options.x_axis_log)
+				f << "set logscale x\n";
+			if(options.y_axis_log)
+				f << "set logscale y\n";
+			f << "plot ";
+			for(unsigned int i=0; i<data.size(); ++i)
+				f << "\"" + temp_data_path + ::toString(i) + ".txt" + "\" using 1:2 title \"" + data[i].key + "\" " + (i < data.size() - 1 ? "," : "");
+			f << "\n";
+
+			if(f.bad())
+				throw Indigo::Exception("Write to '" + temp_path + "' failed.");
+		}
+
+		// Execute Gnuplot
+		/*const int result = */PlatformUtils::execute("pgnuplot " + temp_path);
+
+		//printVar(result);
 	}
-
-	// Execute Gnuplot
-	/*const int result = */PlatformUtils::execute("pgnuplot " + temp_path);
-
-	//printVar(result);
+	catch(FileUtils::FileUtilsExcep& e)
+	{
+		throw Indigo::Exception(e.what());
+	}
+	catch(PlatformUtils::PlatformUtilsExcep& e)
+	{
+		throw Indigo::Exception(e.what());
+	}
 }
 
 
@@ -154,6 +180,12 @@ void Plotter::plot3D(
 			f << "set style data linespoints\n";
 			f << "set xlabel \"" + x_label + "\"\n";
 			f << "set ylabel \"" + y_label + "\"\n";
+
+			if(options.explicit_z_range)
+			{
+				f << "set zrange [" << options.z_start << ":" << options.z_end << "]\n";
+			}
+
 			f << "set title \"" + title + "\"\n";
 			//f << "set encoding iso_8859_1\n"
 			f << "set contour\n";
