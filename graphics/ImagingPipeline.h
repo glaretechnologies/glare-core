790aa13b242c90593492f13f7e211071c6b1a6ce
Nicholas Chapman <nick@indigorenderer.com>
2017-03-10 15:13:33 +0000

    Made ToneMapperDLLAPI object hang onto Render buffer, UInt8Buffer etc.. references. Fixes crash where UInt8Buffer was being deleted while tone mapping was writing to it.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@9112 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- graphics/ImagingPipeline.h
+++ graphics/ImagingPipeline.h
@@ -105,6 +105,8 @@ such as dithering and gamma correction.
 Input colour space is linear sRGB
 Output colour space is non-linear sRGB with the supplied gamma.
 We also assume the output values are not premultiplied alpha.
+
+A lock on uint8_buffer_out is expected to be held by the calling thread if uint8_buffer_out is non-null.
 */
 void toNonLinearSpace(
 	Indigo::TaskManager& task_manager,
