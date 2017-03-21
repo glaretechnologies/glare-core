edebf91a2342de3fb6b864597c6602a48c62ef88
Nicholas Chapman <nick@indigorenderer.com>
2017-03-21 18:54:25 +0000

    Fixed warning.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@9182 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- maths/Matrix4f.cpp
+++ maths/Matrix4f.cpp
@@ -863,7 +863,7 @@ void Matrix4f::test()
 			const float e[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 			const Matrix4f m(e);
 			const float e2[16] ={ 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-			const Matrix4f m2(e);
+			const Matrix4f m2(e2);
 
 			int N = 1000000;
 			float sum = 0.0f;
@@ -887,7 +887,7 @@ void Matrix4f::test()
 			const float e[16] ={ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 			const Matrix4f m(e);
 			const float e2[16] ={ 5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-			const Matrix4f m2(e);
+			const Matrix4f m2(e2);
 
 			int N = 1000000;
 			float sum = 0.0f;
