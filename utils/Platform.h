9c6d2ca4a6fbb7a6a3dd16d43ca1423ec71724d6
buildbox <buildbox>
2017-03-27 13:11:05 +0000

    * removed definition of C99 int types as vs2010 onwards has stdint.h
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@9210 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- utils/Platform.h
+++ utils/Platform.h
@@ -60,19 +60,7 @@ Copyright Glare Technologies Limited 2014 -
 #endif
 
 
-// Workaround for MSVC not including stdint.h for fixed-width int types
-#ifdef _MSC_VER
-typedef signed __int8		int8_t;
-typedef signed __int16		int16_t;
-typedef signed __int32		int32_t;
-typedef signed __int64		int64_t;
-typedef unsigned __int8		uint8_t;
-typedef unsigned __int16	uint16_t;
-typedef unsigned __int32	uint32_t;
-typedef unsigned __int64	uint64_t;
-#else
 #include <stdint.h>
-#endif
 
 typedef uint8_t uint8;
 typedef int16_t int16;
