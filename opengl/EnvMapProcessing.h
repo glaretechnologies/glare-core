3dc394a53d6fef05d42301319db03d2d5ca30102
Nicholas Chapman <nick@indigorenderer.com>
2018-04-19 12:57:12 +0000

    Added EnvMapProcessing.h, used for PBR processing.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@10208 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ opengl/EnvMapProcessing.h
@@ -0,0 +1,15 @@
+/*=====================================================================
+EnvMapProcessing.h
+------------------
+Copyright Glare Technologies Limited 2017 -
+=====================================================================*/
+#pragma once
+
+
+#include <string>
+
+
+namespace EnvMapProcessing
+{
+	void run(const std::string& indigo_base_dir);
+}
