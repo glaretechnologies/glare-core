92e83aa05d9d05cb13b73db3edf7757143c20d55
Nicholas Chapman <nick@indigorenderer.com>
2018-04-19 03:51:40 +0000

    Missed the changes from phong_vert_shader.glsl for PBR rendering.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@10205 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- opengl/shaders/phong_vert_shader.glsl
+++ opengl/shaders/phong_vert_shader.glsl
@@ -4,12 +4,14 @@ in vec3 position_in;
 in vec3 normal_in;
 in vec2 texture_coords_0_in;
 
-out vec3 normal; // cam (view) space
+out vec3 normal_cs; // cam (view) space
+out vec3 normal_ws; // world space
 out vec3 pos_cs;
 out vec2 texture_coords;
 #if NUM_DEPTH_TEXTURES > 0
 out vec3 shadow_tex_coords[NUM_DEPTH_TEXTURES];
 #endif
+out vec3 cam_to_pos_ws;
 
 uniform mat4 proj_matrix;
 uniform mat4 model_matrix;
@@ -18,15 +20,19 @@ uniform mat4 normal_matrix;
 #if NUM_DEPTH_TEXTURES > 0
 uniform mat4 shadow_texture_matrix[NUM_DEPTH_TEXTURES];
 #endif
+uniform vec3 campos_ws;
 
 
 void main()
 {
 	gl_Position = proj_matrix * (view_matrix * (model_matrix * vec4(position_in, 1.0)));
 
+	vec3 pos_ws = (model_matrix  * vec4(position_in, 1.0)).xyz;
+	cam_to_pos_ws = pos_ws - campos_ws;
 	pos_cs = (view_matrix * (model_matrix  * vec4(position_in, 1.0))).xyz;
 
-	normal = (view_matrix * (normal_matrix * vec4(normal_in, 0.0))).xyz;
+	normal_ws = (normal_matrix * vec4(normal_in, 0.0)).xyz;
+	normal_cs = (view_matrix * (normal_matrix * vec4(normal_in, 0.0))).xyz;
 
 	texture_coords = texture_coords_0_in;
 
