efc2a98a5450ad1b143764ccf7ebfc36974a5cde
Nicholas Chapman <nick@indigorenderer.com>
2019-02-05 03:01:33 +0000

    Fixed some whitespace and comments.  NFC.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@10924 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- opencl/OpenCLContext.cpp
+++ opencl/OpenCLContext.cpp
@@ -13,19 +13,19 @@ Copyright Glare Technologies Limited 2016 -
 
 OpenCLContext::OpenCLContext(cl_platform_id platform_id)
 {
-	// Create command queue
+	// Create context
 	cl_context_properties cps[3] =
-    {
-        CL_CONTEXT_PLATFORM,
+	{
+		CL_CONTEXT_PLATFORM,
 		(cl_context_properties)platform_id,
-        0
-    };
+		0
+	};
 
 	cl_int error_code;
 
 	this->context = ::getGlobalOpenCL()->clCreateContextFromType(
 		cps, // properties
-		CL_DEVICE_TYPE_ALL, // CL_DEVICE_TYPE_CPU, // TEMP CL_DEVICE_TYPE_GPU, // device type
+		CL_DEVICE_TYPE_ALL, // device type
 		NULL, // pfn notify
 		NULL, // user data
 		&error_code
