d083d0192fcc29e5a0a89f61606d758e910bd3ca
Nicholas Chapman <nick@indigorenderer.com>
2019-04-05 10:17:05 +0000

    Put grid noise function defs in header so they can be easily inlined.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@10995 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- graphics/GridNoise.h
+++ graphics/GridNoise.h
@@ -7,6 +7,9 @@ Generated at 2011-06-22 22:23:03 +0100
 #pragma once
 
 
+#include "../maths/mathstypes.h"
+
+
 /*=====================================================================
 GridNoise
 -------------------
@@ -16,8 +19,77 @@ namespace GridNoise
 {
 	void generateData();
 
-	float eval(float x, float y, float z);
-	float eval(float x, float y, float z, float w);
-	float eval(int x, int y, int z);
-	float eval(int x, int y, int z, int w);
+	inline float eval(float x, float y, float z);
+	inline float eval(float x, float y, float z, float w);
+	inline float eval(int x, int y, int z);
+	inline float eval(int x, int y, int z, int w);
+}
+
+
+extern const uint8 p_x[256];
+extern const uint8 p_y[256];
+extern const uint8 p_z[256];
+extern const uint8 p_w[256];
+extern const float data[256];
+
+
+/*
+See 'Texturing and Modelling, a Procedural Approach', page 69 - 'Lattice Noises'.
+which gives the code
+
+int i = perm(x + perm(y + perm(z)));
+return data[i];
+
+However the XOR approach used below both looks better and is slightly faster.
+
+NOTE: this stuff can be vectorised when we know we have SSE4 (floorps, andps, etc..)
+*/
+float GridNoise::eval(int x, int y, int z)
+{
+	const int hash_x  = p_x[x & 0xFF];
+	const int hash_y  = p_y[y & 0xFF];
+	const int hash_z  = p_z[z & 0xFF];
+
+	return data[hash_x ^ hash_y ^ hash_z];
+}
+
+
+float GridNoise::eval(int x, int y, int z, int w)
+{
+	const int hash_x  = p_x[x & 0xFF];
+	const int hash_y  = p_y[y & 0xFF];
+	const int hash_z  = p_z[z & 0xFF];
+	const int hash_w  = p_w[w & 0xFF];
+
+	return data[hash_x ^ hash_y ^ hash_z ^ hash_w];
+}
+
+
+float GridNoise::eval(float xf, float yf, float zf)
+{
+	int x = (int)floor(xf);
+	int y = (int)floor(yf);
+	int z = (int)floor(zf);
+
+	const int hash_x  = p_x[x & 0xFF];
+	const int hash_y  = p_y[y & 0xFF];
+	const int hash_z  = p_z[z & 0xFF];
+
+	return data[hash_x ^ hash_y ^ hash_z];
+}
+
+
+float GridNoise::eval(float xf, float yf, float zf, float wf)
+{
+	int x = (int)floor(xf);
+	int y = (int)floor(yf);
+	int z = (int)floor(zf);
+	int w = (int)floor(wf);
+
+	const int hash_x  = p_x[x & 0xFF];
+	const int hash_y  = p_y[y & 0xFF];
+	const int hash_z  = p_z[z & 0xFF];
+	const int hash_w  = p_w[w & 0xFF];
+
+	return data[hash_x ^ hash_y ^ hash_z ^ hash_w];
 }
