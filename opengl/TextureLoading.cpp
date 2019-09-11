c3616a6b6e4bc8cf56c92f61b0486a465e94b9fa
Nicholas Chapman <nick@indigorenderer.com>
2019-09-11 09:14:48 +0000

    Removed #include "../libs/stb/stb_dxt.h" from TextureLoading.cpp.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@11585 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- opengl/TextureLoading.cpp
+++ opengl/TextureLoading.cpp
@@ -17,11 +17,7 @@ Copyright Glare Technologies Limited 2019 -
 #include "../utils/StringUtils.h"
 #include "../utils/ConPrint.h"
 #include "../utils/ArrayRef.h"
-#define STB_DXT_STATIC 1
-#define STB_DXT_IMPLEMENTATION 1
-#include "../libs/stb/stb_dxt.h"
 #include <vector>
-
 
 
 // Thread-safe
