671f902fba45231bd6557ee16c83ea092aa8b699
Nicholas Chapman <nick@indigorenderer.com>
2019-11-07 11:43:17 +0000

    Trying to fix opengl on Mac.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@11803 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- opengl/IncludeOpenGL.h
+++ opengl/IncludeOpenGL.h
@@ -4,8 +4,9 @@
 // Including gl3w.h results in compile errors due to redefined symbols (or something) on OS X. 
 // However on OS X the OpenGL headers are up-to-date for OpenGL 3 core profile, so we can just use them.
 #if defined(OSX)
-#include <OpenGL/gl.h>
-#include <OpenGL/glext.h>
+//#include <OpenGL/gl.h>
+//#include <OpenGL/glext.h>
+#include <GL/gl3w.h>
 #else
 
 #undef GL_VERSION_1_1 // This seems to be needed to get the compile to work for some reason.
