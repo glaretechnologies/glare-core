f8ea5c264e75008e451aaae77a187ff3867c61b6
Nicholas Chapman <nick@indigorenderer.com>
2020-07-31 14:49:35 +0000

    Handling matrix rotations in BackgroundSettingsControl.cpp. Fixes https://bugs.glaretechnologies.com/issues/274, needs some testing though. Improved Matrix3<Real>::rotationMatrixToAxisAngle().
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@12328 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- maths/matrix3.cpp
+++ maths/matrix3.cpp
@@ -12,6 +12,7 @@ Copyright Glare Technologies Limited 2018 -
 
 // Do explicit template instantiation
 template void Matrix3<float>::rotationMatrixToAxisAngle(Vec3<float>& unit_axis_out, float& angle_out) const;
+template void Matrix3<double>::rotationMatrixToAxisAngle(Vec3<double>& unit_axis_out, double& angle_out) const;
 
 
 template <>
@@ -48,18 +49,79 @@ const std::string Matrix3<float>::toStringPlain() const
 template <class Real>
 void Matrix3<Real>::rotationMatrixToAxisAngle(Vec3<Real>& unit_axis_out, Real& angle_out) const
 {
-	//TEMP: http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToAngle/
-	angle_out = acos((elem(0, 0) + elem(1, 1) + elem(2, 2) - 1) / 2);
+	// See also Quat::fromMatrix in Quat.h which is very similar.
+	// Adapted from Quat::fromMatrix() and http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/
+	
+	const Real m00 = elem(0, 0);
+	const Real m11 = elem(1, 1);
+	const Real m22 = elem(2, 2);
+	const Real tr = m00 + m11 + m22; // Trace
+	if(tr >= 0.0)
+	{
+		const Real s = std::sqrt(tr + 1);
 
-	Real x = (elem(2, 1) - elem(1, 2));
-	Real y = (elem(0, 2) - elem(2, 0));
-	Real z = (elem(1, 0) - elem(0, 1));
+		unit_axis_out = normalise(Vec3<Real>( // NOTE: we could eliminate these normalises with some work.  Using a sin() call might not be a perf win however.
+			(elem(2, 1) - elem(1, 2)), // recip_2s applied to all vector elements is not needed due to normalise.
+			(elem(0, 2) - elem(2, 0)),
+			(elem(1, 0) - elem(0, 1))
+		));
 
-	unit_axis_out = normalise(Vec3<Real>(x, y, z));
+		// quaternion w = s/2 = cos(angle/2)
+		// acos(s/2) = angle/2
+		// angle = 2 acos(s/2)
+		angle_out = 2 * std::acos(s * (Real)0.5);
+	}
+	else if(m00 > m11 && m00 > m22)
+	{
+		const Real s = std::sqrt(1 + m00 - m11 - m22);
+		const Real recip_2s = (Real)0.5 / s;
+		
+		unit_axis_out = normalise(Vec3<Real>(
+			s * (Real)0.5,
+			(elem(0, 1) + elem(1, 0)) * recip_2s,
+			(elem(0, 2) + elem(2, 0)) * recip_2s
+		));
+
+		// w = (elem(2, 1) - elem(1, 2)) * 0.5 / s = cos(angle/2)
+		// acos((elem(2, 1) - elem(1, 2)) * 0.5 / s) = angle/2
+		// angle = 2 * acos((elem(2, 1) - elem(1, 2)) * 0.5 / s)
+		angle_out = 2 * std::acos((elem(2, 1) - elem(1, 2)) * recip_2s);
+	}
+	else if(m11 > m22)
+	{
+		const Real s = std::sqrt(1 + m11 - m00 - m22);
+		const Real recip_2s = (Real)0.5 / s;
+
+		unit_axis_out = normalise(Vec3<Real>(
+			(elem(0, 1) + elem(1, 0)) * recip_2s,
+			s * (Real)0.5,
+			(elem(1, 2) + elem(2, 1)) * recip_2s
+		));
+
+		angle_out = 2 * std::acos((elem(0, 2) - elem(2, 0)) * recip_2s);
+	}
+	else
+	{
+		// m22 > m00 > m11
+		const Real s = std::sqrt(1 + m22 - m00 - m11);
+		const Real recip_2s = (Real)0.5 / s;
+
+		unit_axis_out = normalise(Vec3<Real>(
+			(elem(0, 2) + elem(2, 0)) * recip_2s,
+			(elem(1, 2) + elem(2, 1)) * recip_2s,
+			s*(Real)0.5
+		));
+
+		angle_out = 2 * std::acos((elem(1, 0) - elem(0, 1)) * recip_2s);
+	}
 }
 
 
 #if BUILD_TESTS
+
+
+#include "PCG32.h"
+#include "../utils/ConPrint.h"
 
 
 template <>
@@ -97,6 +159,33 @@ void Matrix3<float>::test()
 		m.rotationMatrixToAxisAngle(new_axis, new_angle);
 		testAssert(epsEqual(axis, new_axis));
 		testAssert(epsEqual(angle, new_angle));
+	}
+
+	{
+		PCG32 rng(1);
+
+		for(int i=0; i<1000; ++i)
+		{
+			// Generate a random rot matrix.
+			const Vec3f axis = normalise(Vec3f(-1 + 2 * rng.unitRandom(), -1 + 2 * rng.unitRandom(), -1 + 2 * rng.unitRandom()));
+			const float angle = rng.unitRandom() * Maths::get2Pi<float>();
+			const Matrix3f rot_matrix = Matrix3f::rotationMatrix(axis, angle);
+
+			// Convert to axis/angle
+			Vec3f new_axis;
+			float new_angle;
+			rot_matrix.rotationMatrixToAxisAngle(new_axis, new_angle);
+
+			testAssert(new_axis.isUnitLength());
+			
+			// Convert back to a rot matrix and compare with original rot matrix.
+			const Matrix3f rot_matrix_2 = Matrix3f::rotationMatrix(new_axis, new_angle);
+
+			//conPrint("--------rot_matrix  ------------\n" + rot_matrix.toString());
+			//conPrint("--------rot_matrix_2------------\n" + rot_matrix_2.toString());
+
+			testAssert(epsMatrixEqual(rot_matrix, rot_matrix_2, 5.0e-4f));
+		}
 	}
 }
 
