48d21c93e7454d6fb4941086d17de676de50b0b3
Nicholas Chapman <nick@indigorenderer.com>
2020-10-22 03:10:33 +0000

    * Made flood fill distance for lightmaps 12 pixels, using black instead of green as unfilled pixel colour.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@13911 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- graphics/ImagingPipeline.cpp
+++ graphics/ImagingPipeline.cpp
@@ -1723,53 +1723,73 @@ void runPipeline(
 		//conPrint("Flood filling lightmap...");
 		//Timer timer;
 
-		scratch_state.pre_filled_lightmap = ldr_buffer_out; // Copy the buffer, we don't want to read from it while we are filling in pixels.
-
 		const int xres = (int)ldr_buffer_out.getWidth();
 		const int yres = (int)ldr_buffer_out.getHeight();
+
+		const float alpha_threshold = 0.1; // Pixels with alpha values >= this threshold will have their colour values used.
 
 		for(int y=0; y<yres; ++y)
 			for(int x=0; x<xres; ++x)
 			{
-				const float alpha_threshold = 0.1; // Pixels with alpha values >= this threshold will have their colour values used.
 				// Pixels with an alpha value < this threshold will have their colour values set to the nearest pixel (in the search area) with alpha over this threshold.
 				const float alpha = ldr_buffer_out.getPixel(x, y).x[3];
+
 				if(alpha > alpha_threshold && alpha < 1.0)
 				{
 					ldr_buffer_out.getPixel(x, y) *= (1 / alpha); // Adjust for partial coverage
 				}
-				else if(alpha < alpha_threshold)
+			}
+
+		// Do the actual 'flood fill'
+		for(int i=0; i<12; ++i) // The number of iterations gives the effective distance/radius of the flood fill.  More leaves fewer gaps but is slower to compute.
+		{
+			scratch_state.pre_filled_lightmap = ldr_buffer_out; // Copy the buffer, we don't want to modify it while reading from it.
+
+			for(int y=0; y<yres; ++y)
+				for(int x=0; x<xres; ++x)
 				{
-					// Find a nearby pixel that has some colour values in it (alpha > alpha_threshold).
-					const int r = 2; // search radius in pixels.
-					float closest_d2 = std::numeric_limits<float>::max();
-					Colour4f closest_src_col;
-					for(int sy=myMax(0, y - r); sy<myMin<int>(yres, y + r + 1); ++sy)
-					for(int sx=myMax(0, x - r); sx<myMin<int>(xres, x + r + 1); ++sx) // For example when r=2, we want to read from: x-2, x-1, x, x+1, x+2
+					// Pixels with an alpha value < this threshold will have their colour values set to the nearest pixel (in the search area) with alpha over this threshold.
+					const float alpha = ldr_buffer_out.getPixel(x, y).x[3];
+					if(alpha < alpha_threshold)
 					{
-						const Colour4f src_col = scratch_state.pre_filled_lightmap.getPixel(sx, sy);
-						if(src_col.x[3] > alpha_threshold)
+						// Find a nearby pixel that has some colour values in it (alpha > alpha_threshold).
+						const int r = 1; // search radius in pixels.
+						float closest_d2 = std::numeric_limits<float>::max();
+						Colour4f closest_src_col;
+						for(int sy=myMax(0, y - r); sy<myMin<int>(yres, y + r + 1); ++sy)
+						for(int sx=myMax(0, x - r); sx<myMin<int>(xres, x + r + 1); ++sx) // For example when r=2, we want to read from: x-2, x-1, x, x+1, x+2
 						{
-							const float d2 = (float)(Maths::square(x - sx) + Maths::square(y - sy));
-							if(d2 < closest_d2)
+							const Colour4f src_col = scratch_state.pre_filled_lightmap.getPixel(sx, sy);
+							if(src_col.x[3] > alpha_threshold)
 							{
-								closest_d2 = d2;
-								closest_src_col = src_col;
+								const float d2 = (float)(Maths::square(x - sx) + Maths::square(y - sy));
+								if(d2 < closest_d2)
+								{
+									closest_d2 = d2;
+									closest_src_col = src_col;
+								}
 							}
 						}
-					}
-					if(closest_d2 < std::numeric_limits<float>::max()) // If found a source pixel to copy colour from:
-					{
-						closest_src_col *= (1 / closest_src_col.x[3]); // Adjust for partial coverage
-						ldr_buffer_out.getPixel(x, y) = closest_src_col;
-					}
-					else
-					{
-						ldr_buffer_out.getPixel(x, y).x[0] = 0.0f;
-						ldr_buffer_out.getPixel(x, y).x[1] = 0.5f; // Make green for testing (e.g. testing we aren't doing lightmap reads from the unrendered regions.)
-						ldr_buffer_out.getPixel(x, y).x[2] = 0.0f;
+						if(closest_d2 < std::numeric_limits<float>::max()) // If found a source pixel to copy colour from:
+						{
+							closest_src_col *= (1 / closest_src_col.x[3]); // Adjust for partial coverage  NOTE: shouldn't be needed.
+							ldr_buffer_out.getPixel(x, y) = closest_src_col;
+						}
 					}
 				}
+		}
+
+		for(int y=0; y<yres; ++y)
+			for(int x=0; x<xres; ++x)
+			{
+				// Set unfilled areas to a default colour.
+				//const float alpha = ldr_buffer_out.getPixel(x, y).x[3];
+				//if(alpha < alpha_threshold)
+				//{
+				//	ldr_buffer_out.getPixel(x, y).x[0] = 0.0f;
+				//	ldr_buffer_out.getPixel(x, y).x[1] = 0.5f; // Make green for testing (e.g. testing we aren't doing lightmap reads from the unrendered regions.)
+				//	ldr_buffer_out.getPixel(x, y).x[2] = 0.0f;
+				//}
 
 				ldr_buffer_out.getPixel(x, y).x[3] = 1.f; // Make all pixels have alpha=1.
 			}
