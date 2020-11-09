79a16841d4f7387056cd7819568f4bdcb6fdc543
Nicholas Chapman <nick@indigorenderer.com>
2020-11-09 07:24:15 +0000

    Added some missed files.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@14042 20aa11e4-6e6f-594c-a032-6291ace9ee65

---
+++ utils/EmbreeDeviceHandle.h
@@ -0,0 +1,46 @@
+/*=====================================================================
+EmbreeDeviceHandle.h
+--------------------
+Copyright Glare Technologies Limited 2020 -
+=====================================================================*/
+#pragma once
+
+/*
+* So suppress this warning with #pragma warning:
+
+d:\programming\embree\embree_3.12.1_install-vs2019\include\embree3\rtcore_common.h(234,1): warning C4324: 'RTCPointQuery': structure was padded due to alignment specifier
+
+We'll do this in this separate file for prettiness.
+*/
+
+#ifdef _MSC_VER
+#pragma warning(push, 0) // Disable warnings
+#endif
+
+#include <embree3/rtcore.h>
+
+#ifdef _MSC_VER
+#pragma warning(pop) // Re-enable warnings
+#endif
+
+
+
+class EmbreeDeviceHandle
+{
+public:
+	EmbreeDeviceHandle() : embree_device(NULL) {}
+	EmbreeDeviceHandle(struct RTCDeviceTy* embree_device_) : embree_device(embree_device_) {}
+	~EmbreeDeviceHandle()
+	{
+		if(embree_device)
+			rtcReleaseDevice(embree_device);
+	}
+
+	struct RTCDeviceTy* ptr() { return embree_device; }
+
+private:
+	EmbreeDeviceHandle(const EmbreeDeviceHandle& );
+	EmbreeDeviceHandle& operator = (const EmbreeDeviceHandle& );
+	
+	struct RTCDeviceTy* embree_device;
+};
