8a6bcb8f26390eb6ea5340f73cb556614d1b698a
Nicholas Chapman <nick@indigorenderer.com>
2022-05-03 09:13:24 +0000

    Made KTXDecoder fuzzable. Added a bunch of test KTX files.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@15697 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- graphics/KTXDecoder.cpp
+++ graphics/KTXDecoder.cpp
@@ -1,7 +1,7 @@
 /*=====================================================================
 KTXDecoder.cpp
 --------------
-Copyright Glare Technologies Limited 2020 -
+Copyright Glare Technologies Limited 2022 -
 =====================================================================*/
 #include "KTXDecoder.h"
 
@@ -16,6 +16,7 @@ Copyright Glare Technologies Limited 2020 -
 #include "../utils/StringUtils.h"
 #include "../utils/Vector.h"
 #include "../utils/PlatformUtils.h"
+#include "../utils/BufferViewInStream.h"
 #include <memory.h>
 #include <zstd.h>
 
@@ -28,7 +29,7 @@ Copyright Glare Technologies Limited 2020 -
 
 // Adapted from https://stackoverflow.com/questions/105252/how-do-i-convert-between-big-endian-and-little-endian-values-in-c
 // NOTE: untested
-static uint32 swapByteOrder(const uint32 x)
+static inline uint32 swapByteOrder(const uint32 x)
 {
 	return (x >> 24) |
 		((x << 8) & 0x00FF0000u) |
@@ -37,7 +38,7 @@ static uint32 swapByteOrder(const uint32 x)
 }
 
 
-static inline uint32 readUInt32(FileInStream& file, bool swap_endianness)
+static inline uint32 readUInt32(BufferViewInStream& file, bool swap_endianness)
 {
 	const uint32 x = file.readUInt32();
 	return swap_endianness ? swapByteOrder(x) : x;
@@ -53,7 +54,21 @@ Reference<Map2D> KTXDecoder::decode(const std::string& path)
 {
 	try
 	{
-		FileInStream file(path);
+		MemMappedFile file(path);
+		return decodeFromBuffer(file.fileData(), file.fileSize());
+	}
+	catch(glare::Exception& e)
+	{
+		throw ImFormatExcep(e.what());
+	}
+}
+
+
+Reference<Map2D> KTXDecoder::decodeFromBuffer(const void* data, size_t size)
+{
+	try
+	{
+		BufferViewInStream file(ArrayRef<uint8>((const uint8*)data, size));
 
 		uint8 identifier[12];
 		file.readData(identifier, 12);
@@ -104,12 +119,18 @@ Reference<Map2D> KTXDecoder::decode(const std::string& path)
 		image->gl_base_internal_format = glBaseInternalFormat;
 
 		const uint32 use_num_mipmap_levels = myMax(1u, numberOfMipmapLevels);
+		if(use_num_mipmap_levels > 32)
+			throw glare::Exception("Too many mipmap levels.");
+
 		image->mipmap_level_data.resize(use_num_mipmap_levels);
 
 		// for each mipmap_level in numberOfMipmapLevels
 		for(uint32 lvl = 0; lvl < use_num_mipmap_levels; ++lvl)
 		{
 			const uint32 image_size = readUInt32(file, swap_endianness);
+
+			if(file.getReadIndex() + (size_t)image_size > file.size())
+				throw glare::Exception("MIP image size is too large");
 
 			// Assume there are no array elements (this is not an array texture)
 			glare::AllocatorVector<uint8, 16>& cur_level_data = image->mipmap_level_data[lvl];
@@ -140,22 +161,36 @@ Reference<Map2D> KTXDecoder::decodeKTX2(const std::string& path)
 {
 	try
 	{
-		FileInStream file(path);
+		MemMappedFile file(path);
+		return decodeKTX2FromBuffer(file.fileData(), file.fileSize());
+	}
+	catch(glare::Exception& e)
+	{
+		throw ImFormatExcep(e.what());
+	}
+}
+
+
+Reference<Map2D> KTXDecoder::decodeKTX2FromBuffer(const void* data, size_t size)
+{
+	try
+	{
+		BufferViewInStream file(ArrayRef<uint8>((const uint8*)data, size));
 
 		uint8 identifier[12];
 		file.readData(identifier, 12);
 		if(memcmp(identifier, ktx2_file_id, 12) != 0)
 			throw glare::Exception("Invalid file id");
 
-		const uint32 vkFormat    = file.readUInt32();
-		const uint32 glTypeSize  = file.readUInt32();
-		const uint32 pixelWidth  = file.readUInt32(); // The size of the texture image for level 0, in pixels.
-		const uint32 pixelHeight = file.readUInt32();
-		const uint32 pixelDepth  = file.readUInt32(); // For 2D and cubemap textures, pixelDepth must be 0.
-		const uint32 layerCount  = file.readUInt32(); // layerCount specifies the number of array elements. If the texture is not an array texture, layerCount must equal 0.
-		const uint32 faceCount   = file.readUInt32(); // faceCount specifies the number of cubemap faces
-		const uint32 levelCount  = file.readUInt32(); // levelCount specifies the number of levels in the Mip Level Array
-		const uint32 supercompressionScheme = file.readUInt32();
+		const uint32 vkFormat					= file.readUInt32();
+		/*const uint32 glTypeSize  =*/			  file.readUInt32();
+		const uint32 pixelWidth					= file.readUInt32(); // The size of the texture image for level 0, in pixels.
+		const uint32 pixelHeight				= file.readUInt32();
+		const uint32 pixelDepth					= file.readUInt32(); // For 2D and cubemap textures, pixelDepth must be 0.
+		const uint32 layerCount					= file.readUInt32(); // layerCount specifies the number of array elements. If the texture is not an array texture, layerCount must equal 0.
+		const uint32 faceCount					= file.readUInt32(); // faceCount specifies the number of cubemap faces
+		const uint32 levelCount					= file.readUInt32(); // levelCount specifies the number of levels in the Mip Level Array
+		const uint32 supercompressionScheme		= file.readUInt32();
 
 		if(layerCount > 0)
 			throw glare::Exception("layerCount > 0 not supported.");
@@ -173,12 +208,12 @@ Reference<Map2D> KTXDecoder::decodeKTX2(const std::string& path)
 			throw glare::Exception("Invalid layerCount: " + toString(layerCount));
 
 
-		const uint32 dfdByteOffset = file.readUInt32();
-		const uint32 dfdByteLength = file.readUInt32();
-		const uint32 kvdByteOffset = file.readUInt32();
-		const uint32 kvdByteLength = file.readUInt32();
-		const uint64 sgdByteOffset = file.readUInt64();
-		const uint64 sgdByteLength = file.readUInt64();
+		/*const uint32 dfdByteOffset =*/ file.readUInt32();
+		/*const uint32 dfdByteLength =*/ file.readUInt32();
+		/*const uint32 kvdByteOffset =*/ file.readUInt32();
+		/*const uint32 kvdByteLength =*/ file.readUInt32();
+		/*const uint64 sgdByteOffset =*/ file.readUInt64();
+		/*const uint64 sgdByteLength =*/ file.readUInt64();
 
 		// Read level index
 		struct LevelData
@@ -189,6 +224,8 @@ Reference<Map2D> KTXDecoder::decodeKTX2(const std::string& path)
 		};
 
 		const size_t use_num_mipmap_levels = myMax<size_t>(1, levelCount);
+		if(use_num_mipmap_levels > 32)
+			throw glare::Exception("Too many mipmap levels.");
 		std::vector<LevelData> level_data(use_num_mipmap_levels);
 
 		file.readData(level_data.data(), level_data.size() * sizeof(LevelData));
@@ -245,7 +282,7 @@ Reference<Map2D> KTXDecoder::decodeKTX2(const std::string& path)
 			}
 			else if(supercompressionScheme == 2) // ZSTD compression:
 			{
-				if((level_data[lvl].byteLength >= file.fileSize()) || (file.getReadIndex() + level_data[lvl].byteLength > file.fileSize())) // Check compressed_size is valid, while taking care with wraparound
+				if((level_data[lvl].byteLength >= file.size()) || (file.getReadIndex() + level_data[lvl].byteLength > file.size())) // Check compressed_size is valid, while taking care with wraparound
 					throw glare::Exception("Compressed size is too large.");
 
 				const uint64 decompressed_size = ZSTD_getFrameContentSize(file.currentReadPtr(), level_data[lvl].byteLength);
@@ -256,11 +293,15 @@ Reference<Map2D> KTXDecoder::decodeKTX2(const std::string& path)
 					throw glare::Exception("decompressed_size did not match uncompressedByteLength");
 
 				// Decompress data into 'cur_level_data' buffer.
-				const size_t res = ZSTD_decompress(cur_level_data.data(), decompressed_size, file.currentReadPtr(), /*compressed size=*/level_data[lvl].byteLength);
+				const size_t res = ZSTD_decompress(/*dest=*/cur_level_data.data(), /*dest capacity=*/decompressed_size, /*src=*/file.currentReadPtr(), /*compressed size=*/level_data[lvl].byteLength);
 				if(ZSTD_isError(res))
 					throw glare::Exception("Decompression of buffer failed: " + toString(res));
 				if(res < decompressed_size)
 					throw glare::Exception("Decompression of buffer failed: not enough bytes in result");
+			}
+			else
+			{
+				throw glare::Exception("Unhandled supercompression scheme: " + toString(supercompressionScheme));
 			}
 		}
 
@@ -275,7 +316,11 @@ Reference<Map2D> KTXDecoder::decodeKTX2(const std::string& path)
 
 void KTXDecoder::supercompressKTX2File(const std::string& path_in, const std::string& path_out)
 {
-	CompressedImageRef image = KTXDecoder::decodeKTX2(path_in).downcast<CompressedImage>();
+	CompressedImageRef image;
+	if(hasExtension(path_in, "ktx"))
+		image = KTXDecoder::decode(path_in).downcast<CompressedImage>();
+	else
+		image = KTXDecoder::decodeKTX2(path_in).downcast<CompressedImage>();
 
 	FileOutStream file(path_out);
 	file.writeData(ktx2_file_id, 12);
@@ -348,6 +393,78 @@ void KTXDecoder::supercompressKTX2File(const std::string& path_in, const std::st
 }
 
 
+void KTXDecoder::writeSupercompressKTX2File(int w, int h, const std::vector<std::vector<uint8> >& level_image_data, const std::string& path_out)
+{
+	FileOutStream file(path_out);
+	file.writeData(ktx2_file_id, 12);
+
+	file.writeUInt32(VK_FORMAT_BC6H_UFLOAT_BLOCK); // vkFormat
+	file.writeUInt32(1); // glTypeSize: For formats whose Vulkan names have the suffix _BLOCK it must equal 1
+	file.writeUInt32((uint32)w); // pixelWidth: The size of the texture image for level 0, in pixels.
+	file.writeUInt32((uint32)h); // pixelHeight
+	file.writeUInt32(0); // pixelDepth: For 2D and cubemap textures, pixelDepth must be 0.
+	file.writeUInt32(0); // layerCount: layerCount specifies the number of array elements. If the texture is not an array texture, layerCount must equal 0.
+	file.writeUInt32(1); // faceCount specifies the number of cubemap faces: For non cubemaps this must be 1
+	file.writeUInt32((uint32)level_image_data.size()); // levelCount: levelCount specifies the number of levels in the Mip Level Array
+	file.writeUInt32(2); // supercompressionScheme: Use 2 = zstd
+
+	file.writeUInt32(0); // dfdByteOffset
+	file.writeUInt32(0); // dfdByteLength
+	file.writeUInt32(0); // kvdByteOffset
+	file.writeUInt32(0); // kvdByteLength
+	file.writeUInt64(0); // sgdByteOffset
+	file.writeUInt64(0); // sgdByteLength
+
+	// 80 bytes to here
+
+	//  level index
+	struct LevelData
+	{
+		uint64 byteOffset; // The offset from the start of the file of the first byte of image data for mip level p.
+		uint64 byteLength; // The total size of the data for supercompressed mip level p.
+		uint64 uncompressedByteLength; // levels[p].uncompressedByteLength is the number of bytes of pixel data in LOD levelp after reflation from supercompression.
+	};
+
+	std::vector<LevelData> level_data(1);
+
+	const size_t mip_level_byte_start = file.getWriteIndex() + level_data.size() * sizeof(LevelData);
+	size_t level_byte_write_i = mip_level_byte_start;
+
+	js::Vector<uint8, 16> compressed_data;
+	for(int i=(int)level_image_data.size() - 1; i>=0; --i)
+	{
+		const std::vector<uint8>& level_i_data = level_image_data[i];
+
+		// Compress the buffer with zstandard
+		const size_t compressed_bound = ZSTD_compressBound(level_i_data.size());
+
+		const size_t compressed_data_write_i = compressed_data.size();
+		compressed_data.resize(compressed_data.size() + compressed_bound); // Resize to be large enough to hold compressed_bound additional bytes.
+		
+		const size_t compressed_size = ZSTD_compress(/*dest=*/compressed_data.data() + compressed_data_write_i, /*dst capacity=*/compressed_bound, level_i_data.data(), level_i_data.size(),
+			ZSTD_CLEVEL_DEFAULT // compression level
+		);
+		if(ZSTD_isError(compressed_size))
+			throw glare::Exception("Compression failed: " + toString(compressed_size));
+
+		// Trim compressed_data
+		compressed_data.resize(compressed_data_write_i + compressed_size);
+
+		level_data[i].byteOffset = level_byte_write_i;
+		level_data[i].byteLength = compressed_size;
+		level_data[i].uncompressedByteLength = level_i_data.size();
+
+		level_byte_write_i += compressed_size;
+	}
+
+	// Write level index
+	file.writeData(level_data.data(), level_data.size() * sizeof(LevelData));
+
+	// Write mipmap level data
+	file.writeData(compressed_data.data(), compressed_data.size());
+}
+
+
 #if BUILD_TESTS
 
 
@@ -356,12 +473,60 @@ void KTXDecoder::supercompressKTX2File(const std::string& path_in, const std::st
 #include "../utils/FileUtils.h"
 
 
+#if 1
+// Command line:
+// C:\fuzz_corpus\ktx N:\indigo\trunk\testfiles\ktx
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+	try
+	{
+		KTXDecoder::decodeFromBuffer(data, size);
+	}
+	catch(glare::Exception&)
+	{
+	}
+
+	try
+	{
+		KTXDecoder::decodeKTX2FromBuffer(data, size);
+	}
+	catch(glare::Exception&)
+	{
+	}
+
+	return 0;  // Non-zero return values are reserved for future use.
+}
+#endif
+
+
 void KTXDecoder::test()
 {
 	conPrint("KTXDecoder::test()");
 
 	try
 	{
+		//----------------------------------- Test KTX files in ktxtest-master  -------------------------------------------
+		{
+			const std::vector<std::string> paths = FileUtils::getFilesInDirWithExtensionFullPathsRecursive(TestUtils::getTestReposDir() + "/testfiles/ktx/ktxtest-master", "ktx");
+			for(size_t i=0; i<paths.size(); ++i)
+			{
+				try
+				{
+					conPrint("Loading '" + paths[i] + "'...");
+					Reference<Map2D> im = KTXDecoder::decode(paths[i]);
+
+					// Make some ktx2 files from these ktx files.
+					// KTXDecoder::supercompressKTX2File(paths[i], TestUtils::getTestReposDir() + "/testfiles/ktx/ktx2_from_ktxtest/" + ::removeDotAndExtension(FileUtils::getFilename(paths[i])) + ".ktx2");
+				}
+				catch(glare::Exception& )
+				{
+
+				}
+			}
+		}
+
+
 		Reference<Map2D> im;
 		
 		//----------------------------------- Test loading KTX files -------------------------------------------
--- graphics/KTXDecoder.h
+++ graphics/KTXDecoder.h
@@ -1,13 +1,14 @@
 /*=====================================================================
 KTXDecoder.h
 ------------
-Copyright Glare Technologies Limited 2020 -
+Copyright Glare Technologies Limited 2022 -
 =====================================================================*/
 #pragma once
 
 
 #include <string>
 #include "../utils/Reference.h"
+#include "../utils/ArrayRef.h"
 class Map2D;
 
 
@@ -24,9 +25,16 @@ public:
 	// throws ImFormatExcep on failure
 	static Reference<Map2D> decode(const std::string& path);
 
+	static Reference<Map2D> decodeFromBuffer(const void* data, size_t size);
+
 	static Reference<Map2D> decodeKTX2(const std::string& path);
 
+	static Reference<Map2D> decodeKTX2FromBuffer(const void* data, size_t size);
+
 	static void supercompressKTX2File(const std::string& path_in, const std::string& path_out);
+
+	//static void writeSupercompressKTX2File(int w, int h, ArrayRef<uint8>& mipmap0_data, const std::string& path_out);
+	static void writeSupercompressKTX2File(int w, int h, const std::vector<std::vector<uint8> >& level_image_data, const std::string& path_out);
 
 	static void test();
 };
