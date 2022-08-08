be47ad214612a4f47d9cdb06159702cd901f88a9
Nicholas Chapman <nick@indigorenderer.com>
2022-08-08 15:49:07 +0000

    trying to use std::atomic in AtomicInt.
    git-svn-id: file:///home/nick/svnrepo/repo/indigo/trunk@16040 20aa11e4-6e6f-594c-a032-6291ace9ee65

--- utils/AtomicInt.h
+++ utils/AtomicInt.h
@@ -1,7 +1,7 @@
 /*=====================================================================
 AtomicInt.h
 -----------
-Copyright Glare Technologies Limited 2021
+Copyright Glare Technologies Limited 2022
 =====================================================================*/
 // Not using pragma once since we copy this file and pragma once is path based
 #ifndef GLARE_ATOMIC_INT_H
@@ -10,9 +10,7 @@ Copyright Glare Technologies Limited 2021
 
 #include "Platform.h"
 #include <assert.h>
-#if defined(_WIN32)
-#include <intrin.h>
-#endif
+#include <atomic>
 
 
 namespace glare
@@ -28,6 +26,7 @@ typedef int32 atomic_int;
 
 ///
 /// Atomic integer.
+/// Note: trying std::atomic to see if it plays nicely with Thread Sanitizer.
 ///
 class AtomicInt
 {
@@ -35,8 +34,7 @@ public:
 	inline AtomicInt(atomic_int val_ = 0) : val(val_) { /*assert(((uint64)this % 8) == 0);*/ }
 	inline ~AtomicInt() {}
 
-
-	inline operator atomic_int() const { return val; }
+	inline operator atomic_int() const;
 
 	inline void operator = (atomic_int val_);
 
@@ -58,17 +56,26 @@ private:
 	//GLARE_DISABLE_COPY(IndigoAtomic);
 
 	// The volatile keyword here is required, otherwise, for example, Visual C++ will hoist the load out of a while loop.
-	volatile atomic_int val;
+	//volatile atomic_int val;
+	volatile std::atomic<int64> val;
 };
+
+
+inline AtomicInt::operator atomic_int() const
+{ 
+	//return val;
+	return val.load(std::memory_order_acquire);
+}
 
 
 inline void AtomicInt::operator = (atomic_int newval)
 {
-#if defined(_WIN32)
-	_InterlockedExchange64(&val, newval);
-#else
-	__sync_lock_test_and_set(&val, newval);
-#endif
+	val.store(newval, std::memory_order_release);
+//#if defined(_WIN32)
+//	_InterlockedExchange64(&val, newval);
+//#else
+//	__sync_lock_test_and_set(&val, newval);
+//#endif
 }
 
 
@@ -85,67 +92,71 @@ inline atomic_int AtomicInt::operator-- (int)
 
 
 //----------------- Define an atomicAdd function ---------------
-#if defined(_WIN32)
-
-#if defined(__x86_64__) || defined(__ia64__) || defined(_M_X64) // If 64-bit:
-
-GLARE_STRONG_INLINE atomic_int atomicAdd(volatile atomic_int* val, const atomic_int delta)
-{
-	return _InterlockedExchangeAdd64(val, delta);
-}
-
-#else // Else 32 bit build:
-
-GLARE_STRONG_INLINE glare_atomic_int atomicAdd(volatile atomic_int* val, const atomic_int delta)
-{
-	return _InterlockedExchangeAdd((volatile long*)val, delta);
-}
-
-#endif
-
-#else // else if !defined(_WIN32):
-
-#if defined(__x86_64__) || defined(__ia64__) || defined(_M_X64) // If 64-bit:
-
-GLARE_STRONG_INLINE int64 atomicAdd(int64 volatile* value, int64 input)
-{
-	return __sync_fetch_and_add(value, input);
-}
-
-#else // Else 32 bit build:
-
-GLARE_STRONG_INLINE int32 atomicAdd(int32 volatile* value, int32 input)
-{  
-	return __sync_fetch_and_add(value, input);
-}
-
-#endif
-
-#endif // !defined(_WIN32)
+// #if defined(_WIN32)
+// 
+// #if defined(__x86_64__) || defined(__ia64__) || defined(_M_X64) // If 64-bit:
+// 
+// GLARE_STRONG_INLINE atomic_int atomicAdd(volatile atomic_int* val, const atomic_int delta)
+// {
+// 	return _InterlockedExchangeAdd64(val, delta);
+// }
+// 
+// #else // Else 32 bit build:
+// 
+// GLARE_STRONG_INLINE glare_atomic_int atomicAdd(volatile atomic_int* val, const atomic_int delta)
+// {
+// 	return _InterlockedExchangeAdd((volatile long*)val, delta);
+// }
+// 
+// #endif
+// 
+// #else // else if !defined(_WIN32):
+// 
+// #if defined(__x86_64__) || defined(__ia64__) || defined(_M_X64) // If 64-bit:
+// 
+// GLARE_STRONG_INLINE int64 atomicAdd(int64 volatile* value, int64 input)
+// {
+// 	return __sync_fetch_and_add(value, input);
+// }
+// 
+// #else // Else 32 bit build:
+// 
+// GLARE_STRONG_INLINE int32 atomicAdd(int32 volatile* value, int32 input)
+// {  
+// 	return __sync_fetch_and_add(value, input);
+// }
+// 
+// #endif
+// 
+// #endif // !defined(_WIN32)
 //----------------- End Define an atomicAdd function ---------------
 
 
 inline atomic_int AtomicInt::increment()
 {
-	return atomicAdd(&val, 1);
+	return val.fetch_add(1, std::memory_order_acq_rel);
+	//return atomicAdd(&val, 1);
 }
 
 
 inline atomic_int AtomicInt::decrement()
 {
-	return atomicAdd(&val, -1);
+	return val.fetch_add(-1, std::memory_order_acq_rel);
+	//return atomicAdd(&val, -1);
 }
 
 
 inline atomic_int AtomicInt::operator += (atomic_int x)
 {
-	return atomicAdd(&val, x);
+	return val.fetch_add(x, std::memory_order_acq_rel);
+	//return atomicAdd(&val, x);
 }
 
 
 inline atomic_int AtomicInt::operator -= (atomic_int x)
 {
-	return atomicAdd(&val, -x);
+	return val.fetch_sub(x, std::memory_order_acq_rel);
+	//return atomicAdd(&val, -x);
 }
 
 
