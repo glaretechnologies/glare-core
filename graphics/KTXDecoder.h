b4a6da0bf0a23f82c233338def35baff960ea0d4
Nicholas Chapman <nick@indigorenderer.com>
2022-11-23 19:37:05 +1300

    Updated URL in comment.

--- graphics/KTXDecoder.h
+++ graphics/KTXDecoder.h
@@ -17,7 +17,7 @@ KTXDecoder
 ----------
 KTX is a simple file format, especially useful for block-compressed textures,
 e.g. for OpenGL rendering.
-See KTX File Format Specification - https://www.khronos.org/opengles/sdk/tools/KTX/file_format_spec/
+See KTX File Format Specification - https://github.khronos.org/KTX-Specification/
 =====================================================================*/
 class KTXDecoder
 {
