9f74b454022ff3fb9a4203e7951480c6d0811fbf
Nicholas Chapman <nick@indigorenderer.com>
2023-10-03 02:30:04 +1300

    water_vert_shader.glsl compile fix.

--- opengl/shaders/water_vert_shader.glsl
+++ opengl/shaders/water_vert_shader.glsl
@@ -53,6 +53,9 @@ void main()
 	material_index        = ob_and_mat_indices[gl_DrawID * 4 + 2];
 	mat4 model_matrix  = per_object_data[per_ob_data_index].model_matrix;
 	mat4 normal_matrix = per_object_data[per_ob_data_index].normal_matrix;
+#else
+	mat4 model_matrix  = per_object_data.model_matrix;
+	mat4 normal_matrix = per_object_data.normal_matrix;
 #endif
 
 #if INSTANCE_MATRICES //-------------------------
@@ -69,18 +72,18 @@ void main()
 	normal_ws = (instance_matrix_in * vec4(normal_in, 0.0)).xyz;
 	normal_cs = (view_matrix * (instance_matrix_in * vec4(normal_in, 0.0))).xyz;
 #else //-------- else if !INSTANCE_MATRICES:
-	gl_Position = proj_matrix * (view_matrix * (per_object_data.model_matrix * vec4(position_in, 1.0)));
+	gl_Position = proj_matrix * (view_matrix * (model_matrix * vec4(position_in, 1.0)));
 
 #if GENERATE_PLANAR_UVS
 	pos_os = position_in;
 #endif
 
-	pos_ws = (per_object_data.model_matrix  * vec4(position_in, 1.0)).xyz;
+	pos_ws = (model_matrix  * vec4(position_in, 1.0)).xyz;
 	cam_to_pos_ws = pos_ws - campos_ws.xyz;
-	pos_cs = (view_matrix * (per_object_data.model_matrix  * vec4(position_in, 1.0))).xyz;
+	pos_cs = (view_matrix * (model_matrix  * vec4(position_in, 1.0))).xyz;
 
-	normal_ws = (per_object_data.normal_matrix * vec4(normal_in, 0.0)).xyz;
-	normal_cs = (view_matrix * (per_object_data.normal_matrix * vec4(normal_in, 0.0))).xyz;
+	normal_ws = (normal_matrix * vec4(normal_in, 0.0)).xyz;
+	normal_cs = (view_matrix * (normal_matrix * vec4(normal_in, 0.0))).xyz;
 #endif //-------------------------
 	//texture_coords = texture_coords_0_in;
 
