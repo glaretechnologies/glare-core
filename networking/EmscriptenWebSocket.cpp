14b3318f4629d1b4e4cd08c475bae3a2158d5d17
Ono-Sendai <nick@indigorenderer.com>
2024-02-05 16:31:39 +1300

    Added EmscriptenWebSocket.

---
+++ networking/EmscriptenWebSocket.cpp
@@ -0,0 +1,197 @@
+/*=====================================================================
+EmscriptenWebSocket.cpp
+-----------------------
+Copyright Glare Technologies Limited 2024 -
+=====================================================================*/
+#include "EmscriptenWebSocket.h"
+
+
+#include "../maths/mathstypes.h"
+#include "../utils/BitUtils.h"
+#include "../utils/Exception.h"
+#include "../utils/ConPrint.h"
+#include "../utils/StringUtils.h"
+#include "../utils/PlatformUtils.h"
+#include "../utils/Lock.h"
+#include "MySocket.h"
+
+
+EmscriptenWebSocket::EmscriptenWebSocket()
+{}
+
+
+static EM_BOOL onOpenCallback(int event_type, const EmscriptenWebSocketOpenEvent* websocket_event, void *user_data)
+{
+	conPrint("onOpenCallback");
+
+	return EM_TRUE; // TODO: This right?
+}
+
+
+static EM_BOOL onErrorCallback(int event_type, const EmscriptenWebSocketErrorEvent* websocket_event, void *user_data)
+{
+	conPrint("onErrorCallback");
+
+	return EM_TRUE; // TODO: This right?
+}
+
+
+static EM_BOOL onCloseCallback(int event_type, const EmscriptenWebSocketCloseEvent* websocket_event, void *user_data)
+{
+	conPrint("onCloseCallback");
+
+	return EM_TRUE; // TODO: This right?
+}
+
+
+static EM_BOOL onMessageCallback(int event_type, const EmscriptenWebSocketMessageEvent* websocket_event, void *user_data)
+{
+	// conPrint("onMessageCallback, enqueuing " + toString(websocket_event->numBytes) + " bytes into read buffer");
+
+	assert(!websocket_event->isText);
+
+	EmscriptenWebSocket* websock = (EmscriptenWebSocket*)user_data;
+	
+	// Buffer data
+	{
+		Lock lock(websock->mutex);
+		websock->read_buffer.pushBackNItems(websocket_event->data, websocket_event->numBytes);
+	}
+	websock->new_data_condition.notifyAll(); // Notify all suspended threads that there are new items in the queue. 
+
+	return EM_TRUE; // TODO: This right?
+}
+
+
+void EmscriptenWebSocket::connect(const std::string& hostname, int port)
+{
+	conPrint("EmscriptenWebSocket::connect()...");
+
+	const std::string URL = "ws://" + hostname + ":" + toString(80/*port*/);
+
+	EmscriptenWebSocketCreateAttributes attr;
+	emscripten_websocket_init_create_attributes(&attr);
+	attr.url = URL.c_str();
+	attr.protocols = "binary";
+	attr.createOnMainThread = EM_FALSE; // TEMP
+
+	socket = emscripten_websocket_new(&attr);
+	if(socket <= 0)
+	{
+		conPrint("EmscriptenWebSocket::connect() failed to create websocket ");
+		throw glare::Exception("Failed to create websocket: " + toString(socket));
+	}
+
+	emscripten_websocket_set_onopen_callback (socket, /*userdata=*/this, onOpenCallback);
+	emscripten_websocket_set_onerror_callback(socket, /*userdata=*/this, onErrorCallback);
+	emscripten_websocket_set_onclose_callback(socket, /*userdata=*/this, onCloseCallback);
+	emscripten_websocket_set_onmessage_callback(socket, /*userdata=*/this, onMessageCallback);
+
+
+	// Block until we are in connected state
+	while(1)
+	{
+		// Get state
+		unsigned short ready_state = 0;
+		emscripten_websocket_get_ready_state(socket, &ready_state);
+		if(ready_state == 0) // Connecting state (see https://developer.mozilla.org/en-US/docs/Web/API/WebSocket/readyState)
+		{
+			PlatformUtils::Sleep(10);
+			//conPrint("In connecting state, waiting...");
+		}
+		else if(ready_state == 1) // Open state
+		{
+			conPrint("In open state");
+			break;
+		}
+		else if(ready_state == 2) // closing state
+			throw glare::Exception("Socket closing while connecting");
+		else if(ready_state == 3) // closed state
+			throw glare::Exception("Socket closed while connecting");
+		else
+			throw glare::Exception("Socket has invalid ready state while connecting");
+	}
+
+	conPrint("EmscriptenWebSocket::connect() done.");
+}
+
+
+size_t EmscriptenWebSocket::readSomeBytes(void* buffer, size_t max_num_bytes)
+{
+	assert(0);
+	return 0;
+}
+
+
+void EmscriptenWebSocket::readTo(void* buffer, size_t readlen)
+{
+	Lock lock(mutex); // Lock read-buffer
+
+	// Suspend thread until read-buffer is large enough.
+	while(read_buffer.size() < readlen)
+		new_data_condition.wait(mutex); 
+
+	read_buffer.popFrontNItems(/*dest=*/(uint8*)buffer, readlen);
+}
+
+
+void EmscriptenWebSocket::writeInt32(int32 x)
+{
+	emscripten_websocket_send_binary(socket, &x, sizeof(int32));
+}
+
+
+void EmscriptenWebSocket::writeUInt32(uint32 x)
+{
+	emscripten_websocket_send_binary(socket, &x, sizeof(uint32));
+}
+
+
+int EmscriptenWebSocket::readInt32()
+{
+	uint32 i;
+	readTo(&i, sizeof(uint32));
+	return bitCast<int32>(i);
+}
+
+
+uint32 EmscriptenWebSocket::readUInt32()
+{
+	uint32 x;
+	readTo(&x, sizeof(uint32));
+	return x;
+}
+
+
+void EmscriptenWebSocket::readData(void* buf, size_t num_bytes)
+{
+	readTo(buf, num_bytes);
+}
+
+
+bool EmscriptenWebSocket::endOfStream()
+{
+	return false;
+}
+
+
+void EmscriptenWebSocket::writeData(const void* data, size_t num_bytes)
+{
+	emscripten_websocket_send_binary(socket, (void*)data, num_bytes);
+}
+
+
+#if BUILD_TESTS
+
+
+#include "../utils/ConPrint.h"
+#include "../utils/TestUtils.h"
+
+
+void EmscriptenWebSocket::test()
+{
+	
+}
+
+
+#endif // BUILD_TESTS
---
+++ networking/EmscriptenWebSocket.h
@@ -0,0 +1,89 @@
+/*=====================================================================
+EmscriptenWebSocket.h
+---------------------
+Copyright Glare Technologies Limited 2024 -
+=====================================================================*/
+#pragma once
+
+
+#if 1
+
+
+#include "SocketInterface.h"
+#include "../utils/CircularBuffer.h"
+#include "../utils/Platform.h"
+#include "../utils/Mutex.h"
+#include "../utils/Condition.h"
+//#include <C:\programming\emsdk\upstream\emscripten\system\include/emscripten/emscripten.h>
+//#include <C:\programming\emsdk\upstream\emscripten\system\include/emscripten/websocket.h>
+#include <emscripten/emscripten.h>
+#include <emscripten/websocket.h>
+
+
+/*=====================================================================
+EmscriptenWebSocket
+-------------------
+
+=====================================================================*/
+class EmscriptenWebSocket : public SocketInterface
+{
+public:
+	EmscriptenWebSocket();
+
+	void connect(const std::string& hostname, int port);
+
+	// Read 1 or more bytes from the socket, up to a maximum of max_num_bytes.  Returns number of bytes read.
+	// Returns zero if connection was closed gracefully
+	size_t readSomeBytes(void* buffer, size_t max_num_bytes) override;
+
+	virtual void ungracefulShutdown() override {}
+
+	virtual void waitForGracefulDisconnect() override {}
+
+	virtual void startGracefulShutdown() override {}
+
+
+	virtual void setNoDelayEnabled(bool enabled) override {} // NoDelay option is off by default.
+	virtual void enableTCPKeepAlive(float period) override {}
+	virtual void setAddressReuseEnabled(bool enabled) override {}
+
+	virtual IPAddress getOtherEndIPAddress() const override { return IPAddress(); }
+	virtual int getOtherEndPort() const override { return 0; }
+
+	
+	bool readable(double timeout_s) override { return read_buffer.nonEmpty(); }
+	bool readable(EventFD& event_fd) override { return read_buffer.nonEmpty(); } // Block until either the socket is readable or the event_fd is signalled (becomes readable).
+	// Returns true if the socket was readable, false if the event_fd was signalled.
+
+
+	//------------------------ InStream ---------------------------------
+	virtual int32 readInt32() override;
+	virtual uint32 readUInt32() override;
+	virtual void readData(void* buf, size_t num_bytes) override;
+	virtual bool endOfStream() override;
+	//------------------------------------------------------------------
+
+	//------------------------ OutStream --------------------------------
+	virtual void writeInt32(int32 x) override;
+	virtual void writeUInt32(uint32 x) override;
+	virtual void writeData(const void* data, size_t num_bytes) override;
+	//------------------------------------------------------------------
+
+	static void test();
+
+private:
+	void readTo(void* buffer, size_t numbytes);
+
+public:
+	mutable Mutex mutex;
+	CircularBuffer<uint8> read_buffer;
+	Condition new_data_condition;
+private:
+	EMSCRIPTEN_WEBSOCKET_T socket;
+};
+
+
+typedef Reference<EmscriptenWebSocket> EmscriptenWebSocketRef;
+
+
+#endif
