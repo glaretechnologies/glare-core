82cf4d79c94f90281b103e4782bb0deccd9580d1
Ono-Sendai <nick@indigorenderer.com>
2025-06-28 00:10:40 +1200

    Compile fix.

--- opengl/TextureLoadingTests.h
+++ opengl/TextureLoadingTests.h
@@ -1,18 +1,19 @@
 /*=====================================================================
 TextureLoadingTests.h
 ---------------------
-Copyright Glare Technologies Limited 2019 -
+Copyright Glare Technologies Limited 2024 -
 =====================================================================*/
 #pragma once
 
 
+#include "OpenGLEngine.h"
 #include <string>
 namespace glare { class TaskManager; }
 
 class TextureLoadingTests
 {
 public:
-	static void test();
+	static void test(const Reference<OpenGLEngine>& opengl_engine);
 
 private:
 };
