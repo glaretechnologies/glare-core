8f113810719f374c06a562f149afd9b6730ffedc
Ono-Sendai <nick@indigorenderer.com>
2026-09-20 14:15:13 +1200

    Removed unneeded splitting of shader source into separate lines.

--- opengl/OpenGLShader.cpp
+++ opengl/OpenGLShader.cpp
@@ -37,39 +37,15 @@ OpenGLShader::OpenGLShader(const std::string& path_, const std::string& version_
 	{
 		const std::string shader_src = FileUtils::readEntireFileTextMode(path);
 
-		std::string processed_src = version_directive + "\n";
-		processed_src += preprocessor_defines;
-		processed_src += "#line 1\n"; // Restart line numbering at one again, for more informative error messages, see https://www.khronos.org/opengl/wiki/Core_Language_(GLSL)#Version ('#line directive')
-		processed_src += shader_src;
+		const std::string version = version_directive + "\n";
+		const string_view line_directive = "#line 1\n"; // Restart line numbering at one again, for more informative error messages, see https://www.khronos.org/opengl/wiki/Core_Language_(GLSL)#Version ('#line directive')
 
-		// Build string pointer and length arrays
-		std::vector<const char*> strings;
-		std::vector<GLint> lengths; // Length of each line, including possible \n at end of line.
-		size_t last_line_start_i = 0;
-		for(size_t i=0; i<processed_src.size(); ++i)
-		{
-			if(processed_src[i] == '\n')
-			{
-				strings.push_back(processed_src.data() + last_line_start_i);
-				const size_t line_len = i - last_line_start_i + 1; // Plus one to include this newline character.
-				assert(line_len >= 1);
-				lengths.push_back((GLint)line_len);
-				last_line_start_i = i + 1; // Next line starts after this newline char.
-			}
-		}
+		const char* shader_strings[] = { version.c_str(),       preprocessor_defines.c_str(),       line_directive.data(),        shader_src.c_str() };
+		const GLint lengths[] =        { (GLint)version.size(), (GLint)preprocessor_defines.size(), (GLint)line_directive.size(), (GLint)shader_src.size() };
 
-		// TEMP: dump full shader to disk
-#if EMSCRIPTEN
-		// conPrint("---------------------------------Dumping OpenGL shader '" + path_ + "'----------------------------------------");
-		// conPrint(processed_src);
-		// conPrint("---------------------------------End OpenGL shader '" + path_ + "'----------------------------------------");
-#else
-		// FileUtils::writeEntireFileTextMode("d:/files/" + FileUtils::getFilename(path) + "_processed.glsl", processed_src);
-#endif
+		glShaderSource(shader, /*count=*/(GLsizei)staticArrayNumElems(shader_strings), shader_strings, lengths);
 
-		glShaderSource(shader, (GLsizei)strings.size(), strings.data(), lengths.data());
-
-		glCompileShader(shader);
+		glCompileShader(shader); // Start compiling shader
 
 		// Don't request compilation log here or check compile status, in order to allow parallel compilation to take place first.
 	}
